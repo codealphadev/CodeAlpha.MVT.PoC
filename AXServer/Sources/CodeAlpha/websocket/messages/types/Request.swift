2bc703c9999836d0498efb519b6abc61f28525aa
Adrian Hupka <adrian@hupka.ventures>
2022-03-07 17:13:38 +0100

    Added custom JSON encoder / decoder to Request object

--- AXServer/Sources/CodeAlpha/websocket/messages/types/Request.swift
+++ AXServer/Sources/CodeAlpha/websocket/messages/types/Request.swift
@@ -11,7 +11,7 @@ enum RequestType: String, Codable {
 
 struct Request<T: Codable>: Codable {
   let requestType: RequestType
-  let payload: T
+  let payload: T?
 
   enum CodingKeys: String, CodingKey {
     case requestType = "request"
@@ -24,5 +24,22 @@ extension ByteBuffer {
     let decoder = JSONDecoder()
     decoder.keyDecodingStrategy = .convertFromSnakeCase
     return try? decoder.decode(Request<T>.self, from: self)
+  }
+}
+
+extension Request {
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    requestType = try container.decode(RequestType.self,
+                                       forKey: .requestType)
+    payload = try container.decodeIfPresent(T.self,
+                                            forKey: .payload)
+  }
+
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+
+    try container.encode(requestType, forKey: .requestType)
+    try container.encodeIfPresent(payload, forKey: .payload)
   }
 }
