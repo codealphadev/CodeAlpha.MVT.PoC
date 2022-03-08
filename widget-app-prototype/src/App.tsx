74d3532021393f4cd0405bca60fb60f036e41173
Adrian Hupka <adrian@hupka.ventures>
2022-03-08 16:02:34 +0100

    Added simple frontend listener that publishes AX messages as OS notifications

--- widget-app-prototype/src/App.tsx
+++ widget-app-prototype/src/App.tsx
@@ -1,40 +1,58 @@
-import React from 'react'
-import logo from './logo.svg'
-import tauriCircles from './tauri.svg'
-import tauriWord from './wordmark.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import tauriCircles from "./tauri.svg";
+import tauriWord from "./wordmark.svg";
+import "./App.css";
+
+import { listen } from "@tauri-apps/api/event";
+import { sendNotification } from "@tauri-apps/api/notification";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="inline-logo">
-          <img src={tauriCircles} className="App-logo rotate" alt="logo" />
-          <img src={tauriWord} className="App-logo smaller" alt="logo" />
-        </div>
-        <a
-          className="App-link"
-          href="https://tauri.studio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tauri
-        </a>
-        <img src={logo} className="App-logo rotate" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  )
+	useEffect(() => {
+		const listenToGlobalEvents = async () => {
+			await listen("ax-messages", (event) => {
+				// event.event is the event name (useful if you want to use a single callback fn for multiple event types)
+				// event.payload is the payload object
+				sendNotification(JSON.stringify(event.event));
+			});
+		};
+
+		// call the function
+		listenToGlobalEvents()
+			// make sure to catch any error
+			.catch(console.error);
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="inline-logo">
+					<img src={tauriCircles} className="App-logo rotate" alt="logo" />
+					<img src={tauriWord} className="App-logo smaller" alt="logo" />
+				</div>
+				<a
+					className="App-link"
+					href="https://tauri.studio"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn Tauri
+				</a>
+				<img src={logo} className="App-logo rotate" alt="logo" />
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+			</header>
+		</div>
+	);
 }
 
-export default App
+export default App;
