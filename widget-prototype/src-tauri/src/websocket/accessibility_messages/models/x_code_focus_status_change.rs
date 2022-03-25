f0a0ef35d61392bfde2a0b0a5f29772ba7fc3612
Adrian Hupka <adrian@hupka.ventures>
2022-03-25 18:33:08 +0100

    WIP: extend notifyXCodeStatusChange to include window dimensions

--- widget-prototype/src-tauri/src/websocket/accessibility_messages/models/x_code_focus_status_change.rs
+++ widget-prototype/src-tauri/src/websocket/accessibility_messages/models/x_code_focus_status_change.rs
@@ -11,4 +11,8 @@ pub enum XCodeFocusElement {
 pub struct XCodeFocusStatusChange {
     pub focus_element_change: XCodeFocusElement,
     pub is_in_focus: bool,
+    pub ui_element_x: f64,
+    pub ui_element_y: f64,
+    pub ui_element_w: f64,
+    pub ui_element_h: f64,
 }
