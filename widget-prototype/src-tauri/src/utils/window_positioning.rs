40a46b582a745ac7ecf3789fb85912de801e4c69
Adrian Hupka <adrian@hupka.ventures>
2022-03-27 22:52:17 +0200

    Introduced 'smart' positioning

--- widget-prototype/src-tauri/src/utils/window_positioning.rs
+++ widget-prototype/src-tauri/src/utils/window_positioning.rs
@@ -11,6 +11,7 @@ struct PayloadBubbleOrientationEvent {
 }
 
 #[tauri::command]
+// Checking if widget's position after being dragged is still valid
 pub fn cmd_update_widget_position<R: tauri::Runtime>(handle: tauri::AppHandle<R>) {
     // Get both app windows which need to be positioned in relation to one another
     let content_window = handle.get_window(&window_controls::AppWindow::Content.to_string());
@@ -22,61 +23,59 @@ pub fn cmd_update_widget_position<R: tauri::Runtime>(handle: tauri::AppHandle<R>
     }
 
     // Reposition Content Window and Widget Window
-    if let Some(content) = content_window {
-        if let Some(widget) = widget_window {
-            let screen = widget.current_monitor().unwrap().unwrap();
-            let screen_scale_factor = screen.scale_factor();
-            let pos_screen = screen.position().to_logical::<f64>(screen_scale_factor);
+    if let (Some(content), Some(widget)) = (content_window, widget_window) {
+        let screen = widget.current_monitor().unwrap().unwrap();
+        let screen_scale_factor = screen.scale_factor();
+        let pos_screen = screen.position().to_logical::<f64>(screen_scale_factor);
 
-            let pos_widget = (widget.outer_position())
+        let pos_widget = (widget.outer_position())
+            .unwrap()
+            .to_logical::<f64>(screen_scale_factor);
+
+        let pos_content = (content.outer_position())
+            .unwrap()
+            .to_logical::<f64>(screen_scale_factor);
+
+        let content_size = LogicalSize::<f64> {
+            width: content
+                .outer_size()
                 .unwrap()
-                .to_logical::<f64>(screen_scale_factor);
-
-            let pos_content = (content.outer_position())
+                .to_logical::<f64>(screen_scale_factor)
+                .width as f64,
+            height: content
+                .outer_size()
                 .unwrap()
-                .to_logical::<f64>(screen_scale_factor);
+                .to_logical::<f64>(screen_scale_factor)
+                .height as f64,
+        };
 
-            let content_size = LogicalSize::<f64> {
-                width: content
-                    .outer_size()
-                    .unwrap()
-                    .to_logical::<f64>(screen_scale_factor)
-                    .width as f64,
-                height: content
-                    .outer_size()
-                    .unwrap()
-                    .to_logical::<f64>(screen_scale_factor)
-                    .height as f64,
-            };
+        let widget_size = LogicalSize::<f64> {
+            width: widget
+                .outer_size()
+                .unwrap()
+                .to_logical::<f64>(screen_scale_factor)
+                .width,
+            height: widget
+                .outer_size()
+                .unwrap()
+                .to_logical::<f64>(screen_scale_factor)
+                .height as f64,
+        };
 
-            let widget_size = LogicalSize::<f64> {
-                width: widget
-                    .outer_size()
-                    .unwrap()
-                    .to_logical::<f64>(screen_scale_factor)
-                    .width,
-                height: widget
-                    .outer_size()
-                    .unwrap()
-                    .to_logical::<f64>(screen_scale_factor)
-                    .height as f64,
-            };
-
-            // only reposition, if widget is too close to upper end of screen
-            if pos_screen.y < (pos_widget.y - content_size.height) {
-                return;
-            }
-
-            let new_pos_widget = LogicalPosition {
-                x: pos_content.x + content_size.width - widget_size.width - POSITIONING_OFFSET_Y,
-                y: pos_screen.y
-                    + content_size.height
-                    + (widget_size.height / 2.)
-                    + POSITIONING_OFFSET_X,
-            };
-
-            let _ = widget.set_position(tauri::Position::Logical(new_pos_widget));
+        // only reposition, if widget is too close to upper end of screen
+        if pos_screen.y < (pos_widget.y - content_size.height) {
+            return;
         }
+
+        let new_pos_widget = LogicalPosition {
+            x: pos_content.x + content_size.width - widget_size.width - POSITIONING_OFFSET_Y,
+            y: pos_screen.y
+                + content_size.height
+                + (widget_size.height / 2.)
+                + POSITIONING_OFFSET_X,
+        };
+
+        let _ = widget.set_position(tauri::Position::Logical(new_pos_widget));
     }
 }
 
@@ -92,66 +91,64 @@ pub fn cmd_update_content_position<R: tauri::Runtime>(handle: tauri::AppHandle<R
     }
 
     // Reposition Content Window and Widget Window
-    if let Some(content) = content_window {
-        if let Some(widget) = widget_window {
-            let screen = widget.current_monitor().unwrap().unwrap();
-            let screen_scale_factor = screen.scale_factor();
-            let pos_screen = screen.position().to_logical::<f64>(screen_scale_factor);
+    if let (Some(content), Some(widget)) = (content_window, widget_window) {
+        let screen = widget.current_monitor().unwrap().unwrap();
+        let screen_scale_factor = screen.scale_factor();
+        let pos_screen = screen.position().to_logical::<f64>(screen_scale_factor);
 
-            let pos_widget = (widget.outer_position())
+        let pos_widget = (widget.outer_position())
+            .unwrap()
+            .to_logical::<f64>(screen_scale_factor);
+
+        let widget_size = LogicalSize::<f64> {
+            width: widget
+                .outer_size()
                 .unwrap()
-                .to_logical::<f64>(screen_scale_factor);
+                .to_logical::<f64>(screen_scale_factor)
+                .width,
+            height: widget
+                .outer_size()
+                .unwrap()
+                .to_logical::<f64>(screen_scale_factor)
+                .height as f64,
+        };
+        let content_size = LogicalSize::<f64> {
+            width: content
+                .outer_size()
+                .unwrap()
+                .to_logical::<f64>(screen_scale_factor)
+                .width as f64,
+            height: content
+                .outer_size()
+                .unwrap()
+                .to_logical::<f64>(screen_scale_factor)
+                .height as f64,
+        };
 
-            let widget_size = LogicalSize::<f64> {
-                width: widget
-                    .outer_size()
-                    .unwrap()
-                    .to_logical::<f64>(screen_scale_factor)
-                    .width,
-                height: widget
-                    .outer_size()
-                    .unwrap()
-                    .to_logical::<f64>(screen_scale_factor)
-                    .height as f64,
-            };
-            let content_size = LogicalSize::<f64> {
-                width: content
-                    .outer_size()
-                    .unwrap()
-                    .to_logical::<f64>(screen_scale_factor)
-                    .width as f64,
-                height: content
-                    .outer_size()
-                    .unwrap()
-                    .to_logical::<f64>(screen_scale_factor)
-                    .height as f64,
-            };
+        let mut new_content_pos = LogicalPosition {
+            x: pos_widget.x + (widget_size.width - content_size.width) + POSITIONING_OFFSET_X,
+            y: pos_widget.y - content_size.height - POSITIONING_OFFSET_Y,
+        };
 
-            let mut new_content_pos = LogicalPosition {
-                x: pos_widget.x + (widget_size.width - content_size.width) + POSITIONING_OFFSET_X,
-                y: pos_widget.y - content_size.height - POSITIONING_OFFSET_Y,
-            };
-
-            // Check if the content would be outside the left end of the screen, if so, flip the content window position horizontally
-            let mut bubble_orientation_right = true;
-            if pos_screen.x > new_content_pos.x {
-                new_content_pos.x = pos_widget.x - POSITIONING_OFFSET_X;
-                bubble_orientation_right = false;
-            }
-
-            // Emit event to content window to update its orientation
-            handle
-                .emit_to(
-                    &window_controls::AppWindow::Content.to_string(),
-                    "evt-bubble-icon-orientation",
-                    PayloadBubbleOrientationEvent {
-                        orientation_right: bubble_orientation_right,
-                    },
-                )
-                .unwrap();
-
-            let _ = content.set_position(tauri::Position::Logical(new_content_pos));
+        // Check if the content would be outside the left end of the screen, if so, flip the content window position horizontally
+        let mut bubble_orientation_right = true;
+        if pos_screen.x > new_content_pos.x {
+            new_content_pos.x = pos_widget.x - POSITIONING_OFFSET_X;
+            bubble_orientation_right = false;
         }
+
+        // Emit event to content window to update its orientation
+        handle
+            .emit_to(
+                &window_controls::AppWindow::Content.to_string(),
+                "evt-bubble-icon-orientation",
+                PayloadBubbleOrientationEvent {
+                    orientation_right: bubble_orientation_right,
+                },
+            )
+            .unwrap();
+
+        let _ = content.set_position(tauri::Position::Logical(new_content_pos));
     }
 }
 
--- widget-prototype/src-tauri/src/utils/window_state_machine.rs
+++ widget-prototype/src-tauri/src/utils/window_state_machine.rs
@@ -1,10 +1,16 @@
-use super::window_controls::{cmd_close_window, cmd_open_window, AppWindow};
+use super::{
+    window_controls::{cmd_close_window, cmd_open_window, AppWindow},
+    window_positioning,
+};
 use crate::{
     utils::xcode_twin::XCodeTwin,
     websocket::{models, types},
 };
 use std::sync::{Arc, Mutex};
-use tauri::{EventHandler, Manager};
+use tauri::{EventHandler, Manager, PhysicalSize};
+
+static EDITOR_NAME: &str = "Xcode";
+static APP_NAME: &str = "app";
 
 // Features:
 // [x] Which windows to load at startup
@@ -19,6 +25,10 @@ pub struct WindowStateMachine {
     listener_xcode_focus_status_change: Option<EventHandler>,
 
     preserve_content_visibility_was_visible: Arc<Mutex<bool>>,
+    last_known_editor_position: Arc<Mutex<Option<tauri::PhysicalPosition<i32>>>>,
+    last_known_editor_size: Arc<Mutex<Option<tauri::PhysicalSize<i32>>>>,
+    last_repositioned_widget_position: Arc<Mutex<Option<tauri::PhysicalPosition<i32>>>>,
+    last_focused_app: Arc<Mutex<Option<String>>>,
 }
 
 impl WindowStateMachine {
@@ -31,31 +41,53 @@ impl WindowStateMachine {
         }
 
         Self {
+            tauri_app_handle: app_handle.clone(),
             listener_app_focus_status: None,
             listener_xcode_focus_status_change: None,
             preserve_content_visibility_was_visible,
-            tauri_app_handle: app_handle.clone(),
+            last_known_editor_position: Arc::new(Mutex::new(None)),
+            last_known_editor_size: Arc::new(Mutex::new(None)),
+            last_repositioned_widget_position: Arc::new(Mutex::new(None)),
+            last_focused_app: Arc::new(Mutex::new(None)),
         }
     }
 
     pub fn setup(&mut self) {
-        // 1. Prepare variables to be moved into closure
+        // Registering listener for a change in Global App Focus
+        // ==================================
+        // 1. Copy variable Arcs to be moved into closure (can't move sef into closure)
         let preserve_content_visibility_was_visible_copy =
             self.preserve_content_visibility_was_visible.clone();
         let tauri_app_handle_copy = self.tauri_app_handle.clone();
+        let last_focused_app_copy = self.last_focused_app.clone();
+
+        // 2. Create listener
+        // Store listener id to be able to safely remove it later
         let listener_app_focus_status =
             self.tauri_app_handle
                 .listen_global("StateEvent-AppFocusState", move |msg| {
+                    // Create Listener-Closure and register it at the App(Handle)
+                    // =========================================================
+                    // Only execute if incoming msg contains a payload
                     if let Some(msg_s) = msg.payload() {
                         let parsed_msg: types::Event = serde_json::from_str(&msg_s).unwrap();
-
+                        // Parse msg as Event of correct type
                         if let types::Event::AppFocusState(payload) = parsed_msg {
-                            if !["app", "XCode"].contains(&&payload.current_app.name.as_str()) {
+                            let _app_name = &tauri_app_handle_copy.package_info().name;
+
+                            // For now, on this listener, only hide widget if neither widget or editor are in focus
+                            if ![APP_NAME, EDITOR_NAME]
+                                .contains(&&payload.current_app.name.as_str())
+                            {
                                 Self::hide_widget_preserve_content(
                                     tauri_app_handle_copy.clone(),
                                     &preserve_content_visibility_was_visible_copy,
                                 );
                             }
+
+                            // Update last focused app
+                            let mut last_focused_app = last_focused_app_copy.lock().unwrap();
+                            *last_focused_app = Some(payload.current_app.name);
                         }
                     }
                 });
@@ -63,22 +95,58 @@ impl WindowStateMachine {
 
         // Registering listener for Editor Focus
         // ==================================
-        // 1. Prepare variables to be moved into closure
+        // 1. Copy variable Arcs to be moved into closure (can't move sef into closure)
         let preserve_content_visibility_was_visible_copy =
             self.preserve_content_visibility_was_visible.clone();
+        let last_known_editor_position_copy = self.last_known_editor_position.clone();
+        let last_known_editor_size_copy = self.last_known_editor_size.clone();
+        let last_repositioned_widget_position_copy = self.last_repositioned_widget_position.clone();
         let tauri_app_handle_copy = self.tauri_app_handle.clone();
+        let last_focused_app_copy = self.last_focused_app.clone();
 
         // 2. Create listener
+        // Store listener id to be able to safely remove it later
         let listener_xcode_focus_status_change =
             self.tauri_app_handle
                 .listen_global("StateEvent-XCodeFocusStatusChange", move |msg| {
+                    // Create Listener-Closure and register it at the App(Handle)
+                    // =========================================================
+                    // Only execute if incoming msg contains a payload
                     if let Some(msg_s) = msg.payload() {
+                        // Parse msg as Event of correct type
                         let parsed_msg: types::Event = serde_json::from_str(&msg_s).unwrap();
-
                         if let types::Event::XCodeFocusStatusChange(payload) = parsed_msg {
+                            // For now, on this listener, only react to focus changes on the Editor
                             if let models::XCodeFocusElement::Editor = payload.focus_element_change
                             {
-                                if payload.is_in_focus {
+                                // Show widget if ...
+                                // 1. Last focused app before receiving this msg was NOT this app
+                                // 2. Editor was focused; restore preserved content window visibility.
+                                let last_focused_app = last_focused_app_copy.lock().unwrap();
+                                if payload.is_in_focus
+                                    && *last_focused_app != Some(APP_NAME.to_string())
+                                {
+                                    let editor_position = tauri::PhysicalPosition {
+                                        x: payload.ui_element_x as i32,
+                                        y: payload.ui_element_y as i32,
+                                    };
+
+                                    let editor_size = tauri::PhysicalSize {
+                                        width: payload.ui_element_w as i32,
+                                        height: payload.ui_element_h as i32,
+                                    };
+
+                                    // 1. Reposition
+                                    Self::smartly_position_widget(
+                                        tauri_app_handle_copy.clone(),
+                                        editor_position,
+                                        last_known_editor_position_copy.clone(),
+                                        editor_size,
+                                        last_known_editor_size_copy.clone(),
+                                        last_repositioned_widget_position_copy.clone(),
+                                    );
+
+                                    // 2. Show Widget
                                     Self::show_widget_preserve_content(
                                         tauri_app_handle_copy.clone(),
                                         &preserve_content_visibility_was_visible_copy,
@@ -123,6 +191,147 @@ impl WindowStateMachine {
             .state::<XCodeTwin>()
             .get_state_xcode_focus_state();
     }
+
+    fn smartly_position_widget(
+        app_handle: tauri::AppHandle,
+        curr_editor_pos: tauri::PhysicalPosition<i32>,
+        prev_editor_pos: Arc<Mutex<Option<tauri::PhysicalPosition<i32>>>>,
+        curr_editor_size: tauri::PhysicalSize<i32>,
+        prev_editor_size: Arc<Mutex<Option<tauri::PhysicalSize<i32>>>>,
+        prev_widget_pos: Arc<Mutex<Option<tauri::PhysicalPosition<i32>>>>,
+    ) {
+        // Lock & Unpack Arcs
+        let mut prev_editor_pos_mutex = prev_editor_pos.lock().unwrap();
+        let mut prev_editor_size_mutex = prev_editor_size.lock().unwrap();
+        let mut prev_widget_pos_mutex = prev_widget_pos.lock().unwrap();
+
+        let widget_window = app_handle.get_window(&AppWindow::Widget.to_string());
+        if let Some(widget_window) = widget_window {
+            if let (Some(prev_editor_pos), Some(prev_editor_size), Some(prev_widget_pos)) = (
+                *prev_editor_pos_mutex,
+                *prev_editor_size_mutex,
+                *prev_widget_pos_mutex,
+            ) {
+                // Get current widget position
+                let curr_widget_pos = (widget_window.outer_position()).unwrap();
+
+                // Calculate differences to previous state
+                let editor_move_dist = calc_move_distance(prev_editor_pos, curr_editor_pos);
+                let editor_resize_dist = calc_resize(prev_editor_size, curr_editor_size);
+
+                // Update states:
+                *prev_editor_pos_mutex = Some(curr_editor_pos.clone());
+                *prev_editor_size_mutex = Some(curr_editor_size.clone());
+
+                // Determine, why we are here!
+                if editor_move_dist.width != 0 || editor_move_dist.height != 0 {
+                    // Case 1: Window was moved
+                    let updated_widget_pos = tauri::PhysicalPosition {
+                        x: prev_widget_pos.x + editor_move_dist.width,
+                        y: prev_widget_pos.y + editor_move_dist.height,
+                    };
+                    *prev_widget_pos_mutex = Some(updated_widget_pos.clone());
+                    let _ =
+                        widget_window.set_position(tauri::Position::Physical(updated_widget_pos));
+                } else if editor_resize_dist.width != 0 || editor_resize_dist.width != 0 {
+                    // Case 2: Window was resized
+                    // For now: do nothing
+                    *prev_widget_pos_mutex = Some(curr_widget_pos.clone());
+                } else {
+                    // Case 3: Window was just refocused
+                    // Determine if widget is within editor extends
+                    if is_pos_within_editor_window(
+                        curr_widget_pos,
+                        curr_editor_pos,
+                        curr_editor_size,
+                    ) {
+                        // Rule: If curr_widget_pos is inside editor dimensions, update prev_widget_pos
+                        *prev_widget_pos_mutex = Some(curr_widget_pos.clone());
+                    } else {
+                        // Rule: If curr_widget_pos is outside editor dimensions, restore prev_widget_pos
+                        let _ =
+                            widget_window.set_position(tauri::Position::Physical(prev_widget_pos));
+                    }
+                }
+
+                window_positioning::cmd_update_content_position(app_handle.clone());
+            } else {
+                // Case: No previous positions known, just position at bottom right cornor (startup)
+                Self::position_widget_bottom_right(
+                    app_handle.clone(),
+                    curr_editor_pos,
+                    curr_editor_size,
+                );
+
+                // Update states
+                *prev_editor_pos_mutex = Some(curr_editor_pos.clone());
+                *prev_editor_size_mutex = Some(curr_editor_size.clone());
+                let curr_widget_pos = (widget_window.outer_position()).unwrap();
+                *prev_widget_pos_mutex = Some(curr_widget_pos.clone());
+            }
+        }
+    }
+
+    fn position_widget_bottom_right(
+        app_handle: tauri::AppHandle,
+        editor_position: tauri::PhysicalPosition<i32>,
+        editor_size: tauri::PhysicalSize<i32>,
+    ) {
+        let widget_window = app_handle.get_window(&AppWindow::Widget.to_string());
+        if widget_window.is_some() {
+            let widget_window = widget_window.unwrap();
+
+            let widget_size = PhysicalSize::<u32> {
+                width: widget_window.outer_size().unwrap().width,
+                height: widget_window.outer_size().unwrap().height,
+            };
+
+            // Get Screen Size
+            let screen = widget_window.current_monitor().unwrap().unwrap();
+            let screen_size = screen.size();
+
+            // Calculate new position
+            // Rule 1: place it in the bottom right corner of the editor
+            // Rule 2: move it inwards x/y by the size of the widget
+            let mut new_widget_pos = tauri::PhysicalPosition {
+                x: editor_position.x + editor_size.width
+                    - 5 * Self::helper_u32_to_i32(widget_size.width),
+                y: editor_position.y + editor_size.height
+                    - 2 * Self::helper_u32_to_i32(widget_size.width),
+            };
+
+            // Prevent Widget from going off-screen
+            if new_widget_pos.x < 0 {
+                new_widget_pos.x = 0;
+            }
+            if new_widget_pos.y < 0 {
+                new_widget_pos.y = 0;
+            }
+            if new_widget_pos.x + Self::helper_u32_to_i32(widget_size.width)
+                > Self::helper_u32_to_i32(screen_size.width)
+            {
+                new_widget_pos.x = Self::helper_u32_to_i32(screen_size.width)
+                    - Self::helper_u32_to_i32(widget_size.width);
+            }
+            if new_widget_pos.y + Self::helper_u32_to_i32(widget_size.height)
+                > Self::helper_u32_to_i32(screen_size.height)
+            {
+                new_widget_pos.y = Self::helper_u32_to_i32(screen_size.height)
+                    - Self::helper_u32_to_i32(widget_size.height);
+            }
+
+            // Set new position
+            let _ = widget_window.set_position(tauri::Position::Physical(new_widget_pos));
+
+            // Update Content Window Position
+            window_positioning::cmd_update_content_position(app_handle.clone());
+        }
+    }
+
+    // Just a helper to convert between i32 and u32; it is safe, because on screens u32 can be expected to be small enough.
+    fn helper_u32_to_i32(v: u32) -> i32 {
+        i32::try_from(v).ok().unwrap()
+    }
 }
 
 impl Drop for WindowStateMachine {
@@ -138,6 +347,91 @@ impl Drop for WindowStateMachine {
         {
             self.tauri_app_handle
                 .unlisten(listener_xcode_focus_status_change);
+        }
+    }
+}
+
+fn is_pos_within_editor_window(
+    pos: tauri::PhysicalPosition<i32>,
+    editor_pos: tauri::PhysicalPosition<i32>,
+    editor_size: tauri::PhysicalSize<i32>,
+) -> bool {
+    let mut x_within = false;
+    let mut y_within = false;
+
+    if pos.x > editor_pos.x && pos.x < (editor_pos.x + editor_size.width) {
+        x_within = true;
+    }
+
+    if pos.y > editor_pos.y && pos.y < (editor_pos.y + editor_size.height) {
+        y_within = true;
+    }
+
+    return x_within && y_within;
+}
+
+fn calc_move_distance(
+    prev_pos: tauri::PhysicalPosition<i32>,
+    new_pos: tauri::PhysicalPosition<i32>,
+) -> tauri::PhysicalSize<i32> {
+    tauri::PhysicalSize {
+        width: new_pos.x - prev_pos.x,
+        height: new_pos.y - prev_pos.y,
+    }
+}
+
+fn calc_resize(
+    prev_size: tauri::PhysicalSize<i32>,
+    new_size: tauri::PhysicalSize<i32>,
+) -> tauri::PhysicalSize<i32> {
+    tauri::PhysicalSize {
+        width: new_size.width - prev_size.width,
+        height: new_size.height - prev_size.height,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[allow(unused_imports)]
+    use pretty_assertions::{assert_eq, assert_ne};
+
+    mod test_position_relative_to_editor_window {
+        use crate::utils::window_state_machine::is_pos_within_editor_window;
+
+        #[test]
+        fn is_outside_left() {
+            assert_eq!(
+                verify_is_position_within_editor_window(tauri::PhysicalPosition { x: 10, y: 70 }),
+                false
+            );
+        }
+
+        #[test]
+        fn is_inside_edge() {
+            // if position is "on the edge" of the editor, count it as "in"
+            assert_eq!(
+                verify_is_position_within_editor_window(tauri::PhysicalPosition { x: 50, y: 50 }),
+                false
+            );
+        }
+
+        #[test]
+        fn is_inside() {
+            assert_eq!(
+                verify_is_position_within_editor_window(tauri::PhysicalPosition { x: 500, y: 80 }),
+                true
+            );
+        }
+
+        fn verify_is_position_within_editor_window(position: tauri::PhysicalPosition<i32>) -> bool {
+            let editor_position = tauri::PhysicalPosition { x: 50, y: 50 };
+            let editor_size = tauri::PhysicalSize {
+                width: 1000,
+                height: 500,
+            };
+
+            is_pos_within_editor_window(position, editor_position, editor_size)
         }
     }
 }
