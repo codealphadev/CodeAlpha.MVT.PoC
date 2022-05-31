e9ab073a0325ddff4f406f880606da94acce14f7
Adrian Hupka <adrian@hupka.ventures>
2022-05-31 13:25:53 +0200

    Improved dragging behavior on app activation.

--- src-tauri/src/ax_interaction/app/callbacks/notification_app_activation.rs
+++ src-tauri/src/ax_interaction/app/callbacks/notification_app_activation.rs
@@ -1,11 +1,15 @@
-use accessibility::{AXAttribute, AXUIElement, AXUIElementAttributes, Error};
+use accessibility::{AXAttribute, AXUIElement, Error};
+use enigo::Enigo;
 
 use crate::{
     ax_interaction::{
         models::app::{AppActivatedMessage, AppDeactivatedMessage},
         AXEventApp, AppObserverState,
     },
-    window_controls::AppWindow,
+    window_controls::{
+        actions::{get_position, get_size},
+        AppWindow,
+    },
 };
 
 /// Notify Tauri that our app has been activated, which means focus has moved to our app from a different application.
@@ -20,29 +24,10 @@ pub fn notifiy_app_activated(
     let name = app_element.attribute(&AXAttribute::title())?;
     let pid = app_element.pid()?;
 
-    // attempt to get the currently focused window
-    let mut app_window: Option<AppWindow> = None;
-    if let Ok(focused_element) = app_element.focused_uielement() {
-        if let Ok(window_element) = focused_element.window() {
-            if let Ok(title) = window_element.title() {
-                #[allow(unused_assignments)]
-                match title.to_string().as_str() {
-                    "CodeAlpha - Guide" => app_window = Some(AppWindow::Content),
-                    "CodeAlpha - Settings" => app_window = Some(AppWindow::Settings),
-                    "CodeAlpha - Analytics" => app_window = Some(AppWindow::Analytics),
-                    "CodeAlpha - Widget" => app_window = Some(AppWindow::Widget),
-                    _ => app_window = Some(AppWindow::None),
-                }
-            } else {
-                app_window = Some(AppWindow::None);
-            }
-        }
-    }
-
     let activation_msg = AppActivatedMessage {
         app_name: name.to_string(),
         pid: pid.try_into().unwrap(),
-        focused_app_window: app_window,
+        focused_app_window: check_focus_on_widget_window(&app_state.app_handle),
     };
 
     let activation_event = AXEventApp::AppActivated(activation_msg);
@@ -76,4 +61,37 @@ pub fn notifiy_app_deactivated(
     deactivation_event.publish_to_tauri(&app_state.app_handle);
 
     Ok(())
+}
+
+/// If the mouse cursor is inside the widget window, return Some(AppWindow::Widget), otherwise return
+/// None
+///
+/// Arguments:
+///
+/// * `app_element`: The AXUIElement of the application.
+/// * `app_state`: &AppObserverState
+///
+/// Returns:
+///
+/// An Option containing the focused AppWindow if mouse cursor is inside the widget window, otherwise None
+fn check_focus_on_widget_window(app_handle: &tauri::AppHandle) -> Option<AppWindow> {
+    let mut app_window: Option<AppWindow> = None;
+    if let (Ok(position), Ok(size)) = (
+        get_position(&app_handle, AppWindow::Widget),
+        get_size(&app_handle, AppWindow::Widget),
+    ) {
+        let cursor_location: (i32, i32) = Enigo::mouse_location();
+
+        if cursor_location.0 >= position.x as i32
+            && cursor_location.0 <= position.x as i32 + size.width as i32
+            && cursor_location.1 >= position.y as i32
+            && cursor_location.1 <= position.y as i32 + size.height as i32
+        {
+            app_window = Some(AppWindow::Widget);
+        }
+    } else {
+        app_window = None;
+    }
+
+    app_window
 }
