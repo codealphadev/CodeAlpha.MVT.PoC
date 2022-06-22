0fd7f8e3d95c7601a322616ada9044dc509395af
Adrian Hupka <adrian@hupka.ventures>
2022-06-22 15:58:38 +0200

    Remove debug statements

--- src-tauri/src/recipes/search_and_replace/rule_match.rs
+++ src-tauri/src/recipes/search_and_replace/rule_match.rs
@@ -24,28 +24,25 @@ impl RuleMatch {
     }
 
     pub fn update_rectangles(&mut self, editor_app_pid: i32) {
-        // Get Editor TextArea UI Element
+        // 1. Get Editor TextArea UI Element
         if let Ok(editor_textarea_ui_element) = focused_uielement_of_app(editor_app_pid) {
             let mut line_match_ranges: Vec<MatchRange> = Vec::new();
 
+            // 2. Break up match range into individual matches that only span one line in the editor
             let mut current_match_index = self.range.index;
             while let Some(line_number) = get_line_number_for_range_index(
                 current_match_index as i64,
                 &editor_textarea_ui_element,
             ) {
-                // Get line length
-                if let Some(current_line_range) = dbg!(get_char_range_of_line(
-                    line_number,
-                    &editor_textarea_ui_element
-                )) {
+                if let Some(current_line_range) =
+                    get_char_range_of_line(line_number, &editor_textarea_ui_element)
+                {
                     let line_match_range = MatchRange {
                         index: current_match_index,
                         length: std::cmp::min(
-                            dbg!(
-                                current_line_range.length
-                                    - (current_match_index - current_line_range.index)
-                            ),
-                            dbg!(self.range.length - (current_match_index - self.range.index)),
+                            current_line_range.length
+                                - (current_match_index - current_line_range.index),
+                            self.range.length - (current_match_index - self.range.index),
                         ),
                     };
 
@@ -60,8 +57,7 @@ impl RuleMatch {
                 }
             }
 
-            dbg!(&line_match_ranges);
-
+            // 3. Calculate rectangles for each line match range; checking if they are wrapped, potentially adding multiple rectangles
             let mut line_match_range_rectangles: Vec<MatchRectangle> = Vec::new();
             for line_match_range in line_match_ranges {
                 // Check if line_match_range actually wraps into multiple lines
@@ -70,14 +66,12 @@ impl RuleMatch {
                     is_text_of_line_wrapped(&line_match_range, &editor_textarea_ui_element)
                 {
                     if !range_is_wrapping {
-                        println!("Line is not wrapped");
                         if let Some(line_match_rect) =
                             get_bounds_of_MatchRange(&line_match_range, &editor_textarea_ui_element)
                         {
                             line_match_range_rectangles.push(line_match_rect);
                         }
                     } else {
-                        println!("Line is wrapped");
                         line_match_range_rectangles.extend(calc_match_rects_for_wrapped_range(
                             wrapped_line_number,
                             &line_match_range,
@@ -86,8 +80,6 @@ impl RuleMatch {
                     }
                 }
             }
-
-            dbg!(line_match_range_rectangles);
         }
     }
 }
@@ -107,14 +99,7 @@ mod tests {
                     "// swift-tools-version:4.0\n\nimport PackageDescription\n\nlet".to_string();
                 let mut rule = SearchRule::new();
                 rule.run(&editor_content, &search_str);
-
-                if let Some(mut matches) = rule.rule_matches {
-                    for single_match in matches.iter_mut() {
-                        (*single_match).update_rectangles(editor_pid);
-                    }
-                } else {
-                    assert!(false);
-                }
+                rule.compute_match_boundaries(editor_pid);
             } else {
                 assert!(false, "Focused UI element is not a textarea");
             }
