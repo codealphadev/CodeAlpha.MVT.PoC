33b287388f05a9b222cfe5dcf830257dde5b2fda
Adrian Hupka <adrian@hupka.ventures>
2022-07-26 14:33:43 +0200

    Added utils method to get git root dir from path

--- src-tauri/src/core_engine/rules/utils/fs_utils.rs
+++ src-tauri/src/core_engine/rules/utils/fs_utils.rs
@@ -2,11 +2,11 @@ use std::{
     fs::{create_dir_all, File},
     io::{Read, Write},
     path::PathBuf,
+    process::Command,
 };
 
 type Result<T> = std::result::Result<T, std::io::Error>;
 
-#[allow(dead_code)]
 /// It creates a directory if it doesn't exist, creates a file in that directory, and writes the text content
 /// to the file.
 ///
@@ -19,6 +19,7 @@ type Result<T> = std::result::Result<T, std::io::Error>;
 /// Returns:
 ///
 /// A Result<PathBuf> to the newly created file.
+#[allow(dead_code)]
 pub fn write_text_to_file(
     app_dir_path: PathBuf,
     file_name: &str,
@@ -64,8 +65,55 @@ fn remove_file(file_path: &PathBuf) -> Result<()> {
     Ok(())
 }
 
+/// Returns the (canonicalized) path to the git root directory this path belongs to.
+/// What it does:
+/// * If the path is not in a git repository, it returns None.
+/// * If the path does point to a location that does not exist, it returns None.
+/// * If the path has a file extension at the end, it is popped off and the path is checked again.
+///
+/// Arguments:
+///
+/// * `dir`: The path to the directory to check. If it is a file path, it is converted to a directory path.
+///
+/// Returns:
+///
+/// An Option containing the `PathBuf` to the git root directory (canonicalized).
+#[allow(dead_code)]
+fn get_git_root_dir(dir: &PathBuf) -> Option<PathBuf> {
+    // Check if the path provided contains a file name and extension at the end; if yes, remove it.
+    let mut dir_path = dir.clone();
+    if dir_path.extension().is_some() {
+        dir_path.pop();
+    }
+
+    let output = Command::new("git")
+        .arg("-C")
+        .arg(dir_path)
+        .arg("rev-parse")
+        .arg("--show-toplevel")
+        .output();
+
+    if let Ok(output) = output {
+        if output.status.success() {
+            let root_dir = if let Ok(root_dir) = String::from_utf8(output.stdout) {
+                root_dir
+            } else {
+                return None;
+            };
+            let root_dir = root_dir.trim_end_matches("\n").to_string();
+            Some(PathBuf::from(root_dir))
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
 #[cfg(test)]
 mod tests_fs_utils {
+
+    use std::fs;
 
     use rand::Rng;
 
@@ -97,5 +145,116 @@ mod tests_fs_utils {
 
         remove_file(&file_path).unwrap();
         assert_ne!(file_path.exists(), true);
+    }
+
+    struct FileSystemSetup {
+        pub git_root_dir_name: PathBuf,
+        pub test_folder_dir: PathBuf,
+        pub test_file_path: PathBuf,
+        pub test_dir_not_existing: PathBuf,
+        pub test_dir_not_a_git_repo: PathBuf,
+    }
+
+    impl FileSystemSetup {
+        pub fn new() -> Self {
+            let mut rng = rand::thread_rng();
+            let random_number: u32 = rng.gen::<u32>();
+            let git_root_dir_name =
+                std::env::temp_dir().join(format!("test_get_git_root_dir-{}", random_number));
+            let test_folder_dir = git_root_dir_name.join("test");
+            let test_file_path = test_folder_dir.join("test_file.txt");
+            let test_dir_not_existing = test_folder_dir.join("test_folder_not_existing");
+
+            // create an empty folder temp folder
+            let _ = Command::new("mkdir")
+                .arg(git_root_dir_name.clone())
+                .output()
+                .expect("failed to execute process");
+
+            assert!(git_root_dir_name.exists());
+
+            // create an empty git repository in a temporary directory
+            let _ = Command::new("git")
+                .arg("-C")
+                .arg(git_root_dir_name.clone())
+                .arg("init")
+                .output()
+                .expect("failed to execute process");
+
+            // create a folder in the git repository
+            let _ = Command::new("mkdir")
+                .arg(test_folder_dir.clone())
+                .output()
+                .expect("failed to execute process");
+
+            assert!(test_folder_dir.exists());
+
+            // create a file in the git repository / test_folder
+            let _ = Command::new("touch")
+                .arg("-a")
+                .arg(test_file_path.clone())
+                .output()
+                .expect("failed to execute process");
+
+            Self {
+                git_root_dir_name,
+                test_folder_dir,
+                test_file_path,
+                test_dir_not_existing,
+                test_dir_not_a_git_repo: std::env::temp_dir(),
+            }
+        }
+    }
+
+    impl Drop for FileSystemSetup {
+        fn drop(&mut self) {
+            // remove the git repository
+            let _ = Command::new("rm")
+                .arg("-rf")
+                .arg(self.git_root_dir_name.clone())
+                .output()
+                .expect("failed to execute process");
+
+            assert!(!self.git_root_dir_name.exists());
+        }
+    }
+
+    #[test]
+    #[ignore]
+    fn test_get_git_root_dir() {
+        let test_resources = FileSystemSetup::new();
+
+        // Failing to get git root dir of non-existing directory
+        // Yields the following message in the terminal: "fatal: cannot change to 'PATH_DIR': No such file or directory"
+        assert_eq!(
+            get_git_root_dir(&test_resources.test_dir_not_existing.clone()),
+            None
+        );
+
+        // Failing to get git root dir of directory that exists but that is not a git repository
+        // Yields the following message in the terminal: "fatal: not a git repository (or any of the parent directories): .git"
+        assert_eq!(
+            get_git_root_dir(&test_resources.test_dir_not_a_git_repo.clone()),
+            None
+        );
+
+        // Successfully get git root dir of a git root dir path
+        assert_eq!(
+            get_git_root_dir(&test_resources.git_root_dir_name.clone()),
+            Some(fs::canonicalize(test_resources.git_root_dir_name.clone()).unwrap())
+        );
+
+        // Successfully get git root dir of a directory that is in a git repository
+        assert_eq!(
+            get_git_root_dir(&test_resources.test_folder_dir.clone()),
+            Some(fs::canonicalize(test_resources.git_root_dir_name.clone()).unwrap())
+        );
+
+        // Successfully get git root dir of a file that is in a git repository
+        // Tests if the function correctly popps the filename + extension at the end of the provided path.
+        assert_eq!(
+            get_git_root_dir(&test_resources.test_file_path.clone()),
+            Some(fs::canonicalize(test_resources.git_root_dir_name.clone()).unwrap())
+        );
     }
 }
