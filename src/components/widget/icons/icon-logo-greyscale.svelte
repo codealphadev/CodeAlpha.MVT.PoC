9776e43b60ac9a3a6f2fa5c3e4dd72c8d8ed0f14
Adrian Hupka <adrian@hupka.ventures>
2022-08-04 16:51:22 +0200

    Updated Widget Icon; included icons for doc-gen and swiftformat.

---
+++ src/components/widget/icons/icon-logo-greyscale.svelte
@@ -0,0 +1,61 @@
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 61 61">
+	<path
+		class="logoSymbol"
+		fill="url(#a)"
+		fill-rule="evenodd"
+		d="M19.12 17.41a13.21 13.21 0 0 1 9.23 2.27l.06.05 9.23 7.21 8.17-6.28a17.63 17.63 0 0 0-26.69-3.25Zm23.2 13.18 10.82-8.33-1.03-2.1A23.4 23.4 0 0 0 31.1 6.97 23.47 23.47 0 0 0 7.69 30.5a23.47 23.47 0 0 0 23.4 23.52 23.4 23.4 0 0 0 20.9-12.94l1.06-2.1-10.74-8.4Zm-4.7 3.61-8.9 6.84-.01.01a13.05 13.05 0 0 1-9.59 2.54 17.62 17.62 0 0 0 26.58-3.07l-8.08-6.32Zm-4.67-3.65L25 24.35a7.32 7.32 0 0 0-4.18-1.3 7.43 7.43 0 0 0-7.4 7.45 7.43 7.43 0 0 0 7.4 7.46 7.3 7.3 0 0 0 4.42-1.48l7.7-5.93Z"
+		clip-rule="evenodd"
+	/>
+	<defs>
+		<linearGradient
+			id="a"
+			x1="49.28"
+			x2="13.18"
+			y1="5.31"
+			y2="48.03"
+			gradientUnits="userSpaceOnUse"
+		>
+			<stop stop-color="#F5F5F5" />
+			<stop offset=".78" stop-color="#828282" />
+		</linearGradient>
+	</defs>
+</svg>
+
+<style>
+	@keyframes vibrating {
+		0% {
+			opacity: 1;
+		}
+		30% {
+			opacity: 1;
+		}
+		50% {
+			opacity: 0.75;
+		}
+		70% {
+			opacity: 1;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	.logoSymbol {
+		/* animation: vibrating 3s ease infinite; */
+		stroke-dasharray: 450;
+		stroke-dashoffset: 450;
+		animation: draw 2s linear forwards;
+
+		animation-name: draw, vibrating;
+		animation-duration: 2000ms, 2500ms;
+		animation-delay: 0ms, 2500ms;
+		animation-timing-function: ease-in, ease;
+		animation-iteration-count: 1, infinite;
+	}
+
+	@keyframes draw {
+		to {
+			stroke-dashoffset: 0;
+		}
+	}
+</style>
--- src/pages/widget.svelte
+++ src/pages/widget.svelte
@@ -2,6 +2,11 @@
 	import WidgetIcon from '../components/widget/widget-icon.svelte';
 	import { invoke } from '@tauri-apps/api/tauri';
 	import { appWindow } from '@tauri-apps/api/window';
+	import WidgetBackground from '../components/widget/widget-background.svelte';
+	import IconLogo from '../components/widget/icons/icon-logo.svelte';
+	import IconSwiftFormat from '../components/widget/icons/icon-swift-format.svelte';
+	import IconDocsGen from '../components/widget/icons/icon-docs-gen.svelte';
+	import IconLogoGreyscale from '../components/widget/icons/icon-logo-greyscale.svelte';
 
 	let ghostClickAlreadyHappened = true;
 
@@ -24,6 +29,17 @@
 </script>
 
 <div class="relative">
-	<WidgetIcon />
+	<WidgetBackground />
+	<div class="absolute bottom-0 right-0 w-12 h-12">
+		<div
+			data-tauri-drag-region
+			on:click={clickAction}
+			class="flex items-center justify-center h-screen"
+		>
+			<div class="w-[36px]">
+				<IconLogo />
+			</div>
+		</div>
+	</div>
 	<div data-tauri-drag-region on:click={clickAction} class="absolute bottom-0 right-0 w-12 h-12" />
 </div>
