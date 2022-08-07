5869a9acd920acd44f70a96a9450890cea302d4f
Adrian Hupka <adrian@hupka.ventures>
2022-08-07 22:58:51 +0200

    Complete implementation TrackingAreasManager

--- src-tauri/src/window_controls/events/models/tracking_area_clicked.rs
+++ src-tauri/src/window_controls/events/models/tracking_area_clicked.rs
@@ -1,11 +1,11 @@
 use serde::{Deserialize, Serialize};
 use ts_rs::TS;
 
-use crate::core_engine::types::RuleMatchCategory;
-
 #[derive(Clone, Debug, Serialize, Deserialize, TS)]
 #[ts(export, export_to = "bindings/window_controls/")]
 pub struct TrackingAreaClickedMessage {
     pub id: uuid::Uuid,
-    pub category: RuleMatchCategory,
+
+    /// The duration in millis from when the tracking area was entered to when the click occurred.
+    pub duration_ms: u64,
 }
--- src-tauri/src/window_controls/events/models/tracking_area_exited.rs
+++ src-tauri/src/window_controls/events/models/tracking_area_exited.rs
@@ -1,11 +1,11 @@
 use serde::{Deserialize, Serialize};
 use ts_rs::TS;
 
-use crate::core_engine::types::RuleMatchCategory;
-
 #[derive(Clone, Debug, Serialize, Deserialize, TS)]
 #[ts(export, export_to = "bindings/window_controls/")]
 pub struct TrackingAreaExitedMessage {
     pub id: uuid::Uuid,
-    pub category: RuleMatchCategory,
+
+    /// The duration in millis from when the tracking area was entered to when tracking area was exited.
+    pub duration_ms: u64,
 }
