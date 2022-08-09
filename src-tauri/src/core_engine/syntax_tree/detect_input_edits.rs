e17935c78d2b500e9019edd7be272ab818d39c50
Adrian Hupka <adrian@hupka.ventures>
2022-08-09 14:38:52 +0200

    Adding tests to 'detected_edits' function.

--- src-tauri/src/core_engine/syntax_tree/detect_input_edits.rs
+++ src-tauri/src/core_engine/syntax_tree/detect_input_edits.rs
@@ -15,6 +15,7 @@ pub struct TextDiff {
     pub added_char_sequence: Vec<char>,
     pub removed_char_sequence: Vec<char>,
     pub start_index: usize,
+    pub start_bytes: usize,
 }
 
 pub fn detect_input_edits(old_str: &String, new_str: &String) -> Vec<InputEdit> {
@@ -23,9 +24,11 @@ pub fn detect_input_edits(old_str: &String, new_str: &String) -> Vec<InputEdit> 
     let mut edits: Vec<TextDiff> = Vec::new();
     let mut detected_edit_option: Option<TextDiff> = None;
     let mut walk_index = 0;
+    let mut bytes_counter = 0;
     for diff in diff::chars(&old_str, &new_str) {
         match diff {
             diff::Result::Left(l) => {
+                // detect edit
                 if let Some(detected_edit) = &mut detected_edit_option {
                     // Check if the previous edit is finished
                     if detected_edit.diff_type == DiffType::Add
@@ -42,6 +45,7 @@ pub fn detect_input_edits(old_str: &String, new_str: &String) -> Vec<InputEdit> 
                         detected_edit.added_char_sequence.clear();
                         detected_edit.removed_char_sequence.clear();
                         detected_edit.start_index = walk_index;
+                        detected_edit.start_bytes = bytes_counter;
                         detected_edit.removed_char_sequence.push(l);
                     } else if detected_edit.diff_type == DiffType::Delete {
                         detected_edit.removed_char_sequence.push(l);
@@ -52,20 +56,30 @@ pub fn detect_input_edits(old_str: &String, new_str: &String) -> Vec<InputEdit> 
                         added_char_sequence: Vec::new(),
                         removed_char_sequence: vec![l],
                         start_index: walk_index,
+                        start_bytes: bytes_counter,
                     });
                 }
 
+                // count bytes
+                bytes_counter += char_bytes(l);
+
                 // print!("|-{}|", l)
             }
-            diff::Result::Both(_, _) => {
+            diff::Result::Both(l, _) => {
+                // detect edit
                 // if we end up at a char which is neither added nor removed we wrap up the current edit
                 if let Some(detected_edit) = &mut detected_edit_option {
                     edits.push(detected_edit.clone());
                     detected_edit_option = None;
                 }
+
+                // count bytes
+                bytes_counter += char_bytes(l);
+
                 // print!("{}", l)
             }
             diff::Result::Right(r) => {
+                // detect edit
                 if let Some(detected_edit) = &mut detected_edit_option {
                     // Check if the previous edit is finished
                     if detected_edit.diff_type == DiffType::Add
@@ -83,8 +97,12 @@ pub fn detect_input_edits(old_str: &String, new_str: &String) -> Vec<InputEdit> 
                         added_char_sequence: vec![r],
                         removed_char_sequence: Vec::new(),
                         start_index: walk_index,
+                        start_bytes: bytes_counter,
                     });
                 }
+
+                // count bytes
+                bytes_counter += char_bytes(r);
 
                 // print!("|+{}|", r)
             }
@@ -98,11 +116,29 @@ pub fn detect_input_edits(old_str: &String, new_str: &String) -> Vec<InputEdit> 
         edits.push(detected_edit.clone());
     }
 
-    // Construct InputEdits from the detected edits
+    construct_InputEdits_from_detected_edits(old_str, new_str, &edits)
+}
+
+fn construct_InputEdits_from_detected_edits(
+    old_str: &String,
+    new_str: &String,
+    detected_edits: &Vec<TextDiff>,
+) -> Vec<InputEdit> {
     let mut input_edits: Vec<InputEdit> = Vec::new();
-    for edit in edits.iter() {
+    for edit in detected_edits.iter() {
         let removed_char_count = edit.removed_char_sequence.len();
+        let removed_bytes: usize = edit
+            .removed_char_sequence
+            .iter()
+            .map(|c| char_bytes(*c))
+            .sum();
+
         let added_char_count = edit.added_char_sequence.len();
+        let added_bytes: usize = edit
+            .added_char_sequence
+            .iter()
+            .map(|c| char_bytes(*c))
+            .sum();
 
         let edit_range_before = match edit.diff_type {
             DiffType::Add => TextRange::new(edit.start_index, 0),
@@ -111,18 +147,14 @@ pub fn detect_input_edits(old_str: &String, new_str: &String) -> Vec<InputEdit> 
         };
 
         let edit_range_after = TextRange::new(edit.start_index, added_char_count);
-
-        // println!("{:?}", edit_range_before);
-        // println!("{:?}", edit_range_after);
-
         if let (Some(old_pts), Some(new_pts)) = (
             edit_range_before.as_StartEndTSPoint(old_str),
             edit_range_after.as_StartEndTSPoint(new_str),
         ) {
             input_edits.push(InputEdit {
-                start_byte: edit.start_index,
-                old_end_byte: edit_range_before.index + edit_range_before.length,
-                new_end_byte: edit_range_after.index + edit_range_after.length,
+                start_byte: edit.start_bytes,
+                old_end_byte: edit.start_bytes + removed_bytes,
+                new_end_byte: edit.start_bytes + added_bytes,
                 start_position: old_pts.0,
                 old_end_position: old_pts.1,
                 new_end_position: new_pts.1,
@@ -130,12 +162,247 @@ pub fn detect_input_edits(old_str: &String, new_str: &String) -> Vec<InputEdit> 
         }
     }
 
-    // println!("{:#?}", input_edits);
-    // println!("===");
-    // println!("");
-    // println!("{:#?}", edits);
-    // println!("===");
-    // println!("");
-
+    println!("{:#?}", detected_edits);
+    println!("===");
+    println!("");
     input_edits
+}
+
+fn char_bytes(c: char) -> usize {
+    let size = c.len_utf8();
+    if size == 3 {
+        print!(
+            "Warning: {} is 3 bytes; we didn't check support for such chars yet.",
+            c
+        );
+    }
+
+    size
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_detect_input_edits_remove_at_end_of_line() {
+        let old_str = "abcdef";
+        let new_str = "abcd";
+        let input_edits = detect_input_edits(&old_str.to_string(), &new_str.to_string());
+        assert_eq!(input_edits.len(), 1);
+        assert_eq!(input_edits[0].start_byte, 4);
+        assert_eq!(input_edits[0].old_end_byte, 6);
+        assert_eq!(input_edits[0].new_end_byte, 4);
+        assert_eq!(input_edits[0].start_position.row, 0);
+        assert_eq!(input_edits[0].old_end_position.row, 0);
+        assert_eq!(input_edits[0].new_end_position.row, 0);
+        assert_eq!(input_edits[0].start_position.column, 4);
+        assert_eq!(input_edits[0].old_end_position.column, 6);
+        assert_eq!(input_edits[0].new_end_position.column, 4);
+    }
+
+    #[test]
+    fn test_detect_input_edits_remove_with_emoji() {
+        let old_str = "abcdef😊a";
+        let new_str = "abcd";
+        let input_edits = detect_input_edits(&old_str.to_string(), &new_str.to_string());
+        assert_eq!(input_edits.len(), 1);
+        assert_eq!(input_edits[0].start_byte, 4);
+        assert_eq!(input_edits[0].old_end_byte, 8 + 3); // 3 additional bytes for the emoji
+        assert_eq!(input_edits[0].new_end_byte, 4);
+        assert_eq!(input_edits[0].start_position.row, 0);
+        assert_eq!(input_edits[0].old_end_position.row, 0);
+        assert_eq!(input_edits[0].new_end_position.row, 0);
+        assert_eq!(input_edits[0].start_position.column, 4);
+        assert_eq!(input_edits[0].old_end_position.column, 8);
+        assert_eq!(input_edits[0].new_end_position.column, 4);
+    }
+
+    #[test]
+    fn test_detect_input_edits_remove_with_emoji_end() {
+        let old_str = "abcdef😊";
+        let new_str = "abcd";
+        let input_edits = detect_input_edits(&old_str.to_string(), &new_str.to_string());
+        assert_eq!(input_edits.len(), 1);
+        assert_eq!(input_edits[0].start_byte, 4);
+        assert_eq!(input_edits[0].old_end_byte, 7 + 3); // 3 additional bytes for the emoji
+        assert_eq!(input_edits[0].new_end_byte, 4);
+        assert_eq!(input_edits[0].start_position.row, 0);
+        assert_eq!(input_edits[0].old_end_position.row, 0);
+        assert_eq!(input_edits[0].new_end_position.row, 0);
+        assert_eq!(input_edits[0].start_position.column, 4);
+        assert_eq!(input_edits[0].old_end_position.column, 7);
+        assert_eq!(input_edits[0].new_end_position.column, 4);
+    }
+
+    #[test]
+    fn test_detect_input_edits_add_with_emoji() {
+        let old_str = "abcdef😊a";
+        let new_str = "abcdef😊aBC";
+        let input_edits = detect_input_edits(&old_str.to_string(), &new_str.to_string());
+        assert_eq!(input_edits.len(), 1);
+        assert_eq!(input_edits[0].start_byte, 8 + 3); // 3 additional bytes for the emoji
+        assert_eq!(input_edits[0].old_end_byte, 8 + 3); // 3 additional bytes for the emoji
+        assert_eq!(input_edits[0].new_end_byte, 10 + 3); // 3 additional bytes for the emoji
+        assert_eq!(input_edits[0].start_position.row, 0);
+        assert_eq!(input_edits[0].old_end_position.row, 0);
+        assert_eq!(input_edits[0].new_end_position.row, 0);
+        assert_eq!(input_edits[0].start_position.column, 8);
+        assert_eq!(input_edits[0].old_end_position.column, 8);
+        assert_eq!(input_edits[0].new_end_position.column, 10);
+    }
+
+    #[test]
+    fn test_detect_input_edits_replace_with_emoji() {
+        let old_str = "abc😊aBCd";
+        let new_str = "abc😊aXXYYYd";
+        let input_edits = detect_input_edits(&old_str.to_string(), &new_str.to_string());
+        assert_eq!(input_edits.len(), 1);
+        assert_eq!(input_edits[0].start_byte, 5 + 3); // 3 additional bytes for the emoji
+        assert_eq!(input_edits[0].old_end_byte, 7 + 3); // 3 additional bytes for the emoji
+        assert_eq!(input_edits[0].new_end_byte, 10 + 3); // 3 additional bytes for the emoji
+        assert_eq!(input_edits[0].start_position.row, 0);
+        assert_eq!(input_edits[0].old_end_position.row, 0);
+        assert_eq!(input_edits[0].new_end_position.row, 0);
+        assert_eq!(input_edits[0].start_position.column, 5);
+        assert_eq!(input_edits[0].old_end_position.column, 7);
+        assert_eq!(input_edits[0].new_end_position.column, 10);
+    }
+
+    #[test]
+    fn test_detect_input_edits_replace_beginning_of_file() {
+        let old_str = "let x = 1; console.log(x);";
+        let new_str = "const x = 1; console.log(x);";
+        let input_edits = detect_input_edits(&old_str.to_string(), &new_str.to_string());
+
+        println!("{:#?}", &input_edits);
+        assert_eq!(input_edits.len(), 1);
+        assert_eq!(input_edits[0].start_byte, 0);
+        assert_eq!(input_edits[0].old_end_byte, 2);
+        assert_eq!(input_edits[0].new_end_byte, 4);
+        assert_eq!(input_edits[0].start_position.row, 0);
+        assert_eq!(input_edits[0].old_end_position.row, 0);
+        assert_eq!(input_edits[0].new_end_position.row, 0);
+        assert_eq!(input_edits[0].start_position.column, 0);
+        assert_eq!(input_edits[0].old_end_position.column, 2);
+        assert_eq!(input_edits[0].new_end_position.column, 4);
+    }
+
+    #[test]
+    fn test_detect_input_edits_replace_in_middle_of_line() {
+        let old_str = "fn test(XXX) {}";
+        let new_str = "fn test(a: u32) {}";
+        let input_edits = detect_input_edits(&old_str.to_string(), &new_str.to_string());
+
+        println!("{:#?}", &input_edits);
+        assert_eq!(input_edits.len(), 1);
+        assert_eq!(input_edits[0].start_byte, 8);
+        assert_eq!(input_edits[0].old_end_byte, 11);
+        assert_eq!(input_edits[0].new_end_byte, 14);
+        assert_eq!(input_edits[0].start_position.row, 0);
+        assert_eq!(input_edits[0].old_end_position.row, 0);
+        assert_eq!(input_edits[0].new_end_position.row, 0);
+        assert_eq!(input_edits[0].start_position.column, 8);
+        assert_eq!(input_edits[0].old_end_position.column, 11);
+        assert_eq!(input_edits[0].new_end_position.column, 14);
+    }
+
+    #[test]
+    fn test_detect_input_edits_replace_whole_line() {
+        let old_str = "fn test0(a: u32) {}\n//row\nfn test2(a: u32) {}";
+        let new_str = "fn test0(a: u32) {}\nfn test1(a: u32) {}\nfn test2(a: u32) {}";
+        let input_edits = detect_input_edits(&old_str.to_string(), &new_str.to_string());
+
+        println!("{:#?}", &input_edits);
+        assert_eq!(input_edits.len(), 1);
+        assert_eq!(input_edits[0].start_byte, 20);
+        assert_eq!(input_edits[0].old_end_byte, 25);
+        assert_eq!(input_edits[0].new_end_byte, 39);
+        assert_eq!(input_edits[0].start_position.row, 1);
+        assert_eq!(input_edits[0].old_end_position.row, 1);
+        assert_eq!(input_edits[0].new_end_position.row, 1);
+        assert_eq!(input_edits[0].start_position.column, 0);
+        assert_eq!(input_edits[0].old_end_position.column, 5);
+        assert_eq!(input_edits[0].new_end_position.column, 19);
+    }
+}
+
+#[cfg(test)]
+mod test_tree_sitter_logic {
+
+    use pretty_assertions::assert_eq;
+    use tree_sitter::{Parser, Point};
+
+    fn setup_swift_parser() -> Parser {
+        let mut parser = Parser::new();
+        parser
+            .set_language(tree_sitter_swift::language())
+            .expect("Swift Language not found");
+
+        parser
+    }
+
+    #[test]
+    fn test_start_end_point_end_newline_char() {
+        let text = "let x = 1; console.log(x);\n";
+        //                |------------------------>| <- end column is zero on row 1
+        //                                            <- end byte is one past the last byte (27), as they are also zero-based
+        let mut swift_parser = setup_swift_parser();
+        let tree = swift_parser.parse(text, None).unwrap();
+
+        let root_node = tree.root_node();
+
+        // println!("{:#?}", root_node.start_position());
+        // println!("{:#?}", root_node.end_position());
+
+        assert_eq!(root_node.start_byte(), 0);
+        assert_eq!(root_node.end_byte(), 27);
+        assert_eq!(root_node.start_position(), Point { row: 0, column: 0 });
+        assert_eq!(root_node.end_position(), Point { row: 1, column: 0 });
+    }
+
+    #[test]
+    fn test_start_end_point_end_no_newline_char() {
+        let text = "let x = 1; console.log(x);";
+        //                |------------------------>| <- end column is one past the last char (26)
+        //                |------------------------>| <- end byte is one past the last byte (26), as they are also zero-based
+        let mut swift_parser = setup_swift_parser();
+        let tree = swift_parser.parse(text, None).unwrap();
+
+        let root_node = tree.root_node();
+
+        // println!("{:#?}", root_node.start_position());
+        // println!("{:#?}", root_node.end_position());
+
+        assert_eq!(root_node.start_byte(), 0);
+        assert_eq!(root_node.end_byte(), 26);
+        assert_eq!(root_node.start_position(), Point { row: 0, column: 0 });
+        assert_eq!(root_node.end_position(), Point { row: 0, column: 26 });
+    }
+
+    #[test]
+    fn test_start_end_point_with_UTF16_chars() {
+        let mut swift_parser = setup_swift_parser();
+
+        let utf16_str = "// 😊\n";
+        let utf8_str = "let x = 1; console.log(x);";
+        let text = format!("{}{}", utf16_str, utf8_str);
+
+        let tree = swift_parser.parse(text, None).unwrap();
+
+        let root_node = tree.root_node();
+
+        // println!("Start Pos: {:#?}", root_node.start_position());
+        // println!("End Pos: {:#?}", root_node.end_position());
+        // println!("Start Byte: {:#?}", root_node.start_byte());
+        // println!("End Byte: {:#?}", root_node.end_byte());
+
+        assert_eq!(root_node.start_byte(), 0);
+        let byte_count = utf8_str.bytes().count() + utf16_str.bytes().count(); // 26 (utf8) + 8 (utf16, emoji is 4 bytes)
+        assert_eq!(root_node.end_byte(), byte_count);
+        assert_eq!(root_node.start_position(), Point { row: 0, column: 0 });
+        assert_eq!(root_node.end_position(), Point { row: 1, column: 26 });
+    }
 }
