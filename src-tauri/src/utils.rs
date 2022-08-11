0ff6d5eb5c0024d049860b0a3cba5a936c18e0cf
Adam Nyberg <adam.nyberg@gmail.com>
2022-08-11 09:22:21 +0200

    Add util to get mintlify documentation

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,4 +1,5 @@
-#![allow(unused)]
+use serde::{Deserialize, Serialize};
+use tauri::async_runtime::block_on;
 /// A file containing utilities that are used/shared across all modules of the project
 
 pub mod geometry {
@@ -96,5 +97,70 @@ pub mod messaging {
                 ChannelList::RuleResults => write!(f, "RuleResults"),
             }
         }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MintlifyResponse {
+    docstring: Option<String>,
+    feedbackId: String,
+    position: String,
+    preview: String,
+    shouldShowFeedback: bool,
+    shouldShowShare: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MintlifyRequest {
+    apiKey: String,
+    code: String,
+    context: Option<String>,
+}
+
+pub fn get_mintlify_documentation(
+    code: String,
+    context: Option<String>,
+) -> Option<MintlifyResponse> {
+    let handle = mintlify_documentation(code, context);
+    let mintlify_response: Result<MintlifyResponse, reqwest::Error> = block_on(handle);
+
+    if let Ok(mintlify_response) = mintlify_response {
+        Some(mintlify_response)
+    } else {
+        None
+    }
+}
+
+async fn mintlify_documentation(
+    code: String,
+    context: Option<String>,
+) -> Result<MintlifyResponse, reqwest::Error> {
+    let req_body = MintlifyRequest {
+        apiKey: "-RWsev7z_qgP!Qinp_8cbmwgP9jg4AQBkfz".to_string(),
+        code,
+        context,
+    };
+
+    let response = reqwest::Client::new()
+        .post("https://europe-west1-codealpha-analyze-text-dev.cloudfunctions.net/analyze-code")
+        .json(&req_body)
+        .send()
+        .await?;
+    let parsed_response = response.json().await?;
+    Ok(parsed_response)
+}
+
+#[cfg(test)]
+mod tests_mintlify {
+
+    use super::get_mintlify_documentation;
+
+    #[test]
+    fn test_get_mintlify_documentation() {
+        let resp = get_mintlify_documentation(
+            "print(\"Hello World\")".to_string(),
+            Some("print(\"Hello World\")".to_string()),
+        );
+        assert!(resp.is_some());
     }
 }
