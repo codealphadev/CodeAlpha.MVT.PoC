8d19d135c8ff861a6ea8f52a6b6d40caff1d72e5
Adrian Hupka <adrian@hupka.ventures>
2022-08-18 12:36:11 +0200

    COD-172: Added dimension recomputation of code overlay to EventWindowControls (needs rebase fixing)

--- src/pages/code-overlay.svelte
+++ src/pages/code-overlay.svelte
@@ -2,7 +2,13 @@
 	import { listen, Event } from '@tauri-apps/api/event';
 	import type { ChannelList } from '../../src-tauri/bindings/ChannelList';
 	import type { EventDocsGeneration } from '../../src-tauri/bindings/features/docs_generation/EventDocsGeneration';
+	import type { EventWindowControls } from '../../src-tauri/bindings/window_controls/EventWindowControls';
 	import type { MatchRectangle } from '../../src-tauri/bindings/rules/utils/MatchRectangle';
+
+	import type { LogicalSize } from '../../src-tauri/bindings/geometry/LogicalSize';
+	import type { LogicalPosition } from '../../src-tauri/bindings/geometry/LogicalPosition';
+	import type { LogicalFrame } from '../../src-tauri/bindings/geometry/LogicalFrame';
+	import type { RuleName } from '../../src-tauri/bindings/rules/RuleName';
 	import type { CodeAnnotationMessage } from '../../src-tauri/bindings/features/docs_generation/CodeAnnotationMessage';
 	import DocsAnnotations from '../components/code-overlay/docs-generation/docs-annotations.svelte';
 	import BracketHighlight from '../components/code-overlay/bracket-highlight/bracket-highlight.svelte';
@@ -17,6 +23,24 @@
 			code_overlay_rectangle = payload;
 		});
 
+		let WindowControlsChannel: ChannelList = 'EventWindowControls';
+		await listen(WindowControlsChannel, (event) => {
+			const event_window_controls = JSON.parse(event.payload as string) as EventWindowControls;
+
+			switch (event_window_controls.event) {
+				case 'CodeOverlayDimensionsUpdate':
+					const updated_code_overlay_dim = event_window_controls.payload as unknown as LogicalFrame;
+					outerSize = updated_code_overlay_dim.size;
+					outerPosition = updated_code_overlay_dim.origin;
+					height = updated_code_overlay_dim.size.height;
+
+					compute_rule_rects();
+
+					break;
+				default:
+					break;
+			}
+		});
 		// Listener for docs generation feature
 		let DocsGenerationChannel: ChannelList = 'EventDocsGeneration';
 		await listen(DocsGenerationChannel, (event) => {
