4943581d3d7852449e81aee67bb653279a389f2c
Adrian Hupka <adrian@hupka.ventures>
2022-08-19 17:39:43 +0200

    COD-172: Bugfix: AX Observer for our app did not properly register.

--- src-tauri/src/ax_interaction/app/observer_app.rs
+++ src-tauri/src/ax_interaction/app/observer_app.rs
@@ -43,29 +43,27 @@ fn create_observer_and_add_notifications() -> Result<(), Error> {
     let pid: i32 = std::process::id().try_into().unwrap();
 
     // 1. Create AXObserver
-    let app_observer = AXObserver::new(pid, callback_app_notifications);
+    let mut app_observer = AXObserver::new(pid, callback_app_notifications)?;
     let ui_element = AXUIElement::application(pid);
 
-    if let Ok(mut app_observer) = app_observer {
-        store_registered_ax_observer(pid, ObserverType::App, &app_observer);
+    store_registered_ax_observer(pid, ObserverType::App, &app_observer);
 
-        // 2. Start AXObserver before adding notifications
-        app_observer.start();
+    // 2. Start AXObserver before adding notifications
+    app_observer.start();
 
-        // 3. Add notifications
-        for notification in OBSERVER_NOTIFICATIONS.iter() {
-            let _ = app_observer.add_notification(
-                notification,
-                &ui_element,
-                AppObserverState {
-                    app_handle: app_handle(),
-                },
-            );
-        }
-
-        // 4. Kick of RunLoop on this thread
-        CFRunLoop::run_current();
+    // 3. Add notifications
+    for notification in OBSERVER_NOTIFICATIONS.iter() {
+        let _ = app_observer.add_notification(
+            notification,
+            &ui_element,
+            AppObserverState {
+                app_handle: app_handle(),
+            },
+        );
     }
+
+    // 4. Kick of RunLoop on this thread
+    CFRunLoop::run_current();
 
     Ok(())
 }
