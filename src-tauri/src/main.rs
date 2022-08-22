574e304797462857a652e298854d8aa891414275
Adrian Hupka <adrian@hupka.ventures>
2022-08-22 09:43:59 +0200

    COD-172: Activated deadlock detection for Mutexes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -79,6 +79,9 @@ fn main() {
                 }
             });
 
+            // Spin up a thread to detect potential Mutex deadlocks.
+            deadlock_detection();
+
             Ok(())
         })
         .system_tray(system_tray)
@@ -114,5 +117,29 @@ fn main() {
             api.prevent_exit();
         }
         _ => {}
+    });
+}
+
+fn deadlock_detection() {
+    use parking_lot::deadlock;
+    use std::thread;
+    use std::time::Duration;
+
+    // Create a background thread which checks for deadlocks every 2s
+    thread::spawn(move || loop {
+        thread::sleep(Duration::from_secs(2));
+        let deadlocks = deadlock::check_deadlock();
+        if deadlocks.is_empty() {
+            continue;
+        }
+
+        println!("{} deadlocks detected", deadlocks.len());
+        for (i, threads) in deadlocks.iter().enumerate() {
+            println!("Deadlock #{}", i);
+            for t in threads {
+                println!("Thread Id {:#?}", t.thread_id());
+                println!("{:#?}", t.backtrace());
+            }
+        }
     });
 }
