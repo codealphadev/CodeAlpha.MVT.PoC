d8c7629085a1aa8fb98f2bea97a3380a9bb925aa
Adam Nyberg <adam.nyberg@gmail.com>
2022-08-25 15:23:21 +0200

    COD-240: Refactor swift format async task

--- src-tauri/src/core_engine/features/formatter/swift_formatter.rs
+++ src-tauri/src/core_engine/features/formatter/swift_formatter.rs
@@ -1,8 +1,5 @@
 use std::path::Path;
-use tauri::{
-    api::process::{Command, CommandEvent},
-    async_runtime::block_on,
-};
+use tauri::api::process::{Command, CommandEvent};
 
 use crate::{
     ax_interaction::{
@@ -21,85 +18,81 @@ pub fn format_swift(
     selected_text_range: &TextRange,
     pid: i32,
     app_handle: &tauri::AppHandle,
-) -> Option<XcodeText> {
-    let formatted_content = if let Some(formatted_content) = format_swift_file(file_path) {
-        formatted_content
-    } else {
-        return None;
-    };
+) {
+    let file_path_move = file_path.clone();
+    let old_text_move = old_text.clone();
+    let selected_text_range_move = selected_text_range.clone();
+    let app_handle_move = app_handle.clone();
+    tauri::async_runtime::spawn(async move {
+        let new_text = if let Some(formatted_content) = format_file(&file_path_move).await {
+            XcodeText::from_str(&formatted_content)
+        } else {
+            return;
+        };
 
-    if formatted_content == *old_text {
-        return None;
-    }
+        if new_text == old_text_move {
+            return;
+        }
 
-    // Get position of selected text
-    let mut scroll_delta = None;
-    if let Some(editor_textarea_ui_element) = get_textarea_uielement(pid) {
-        // Get the dimensions of the textarea viewport
-        if let Ok(textarea_viewport) = derive_xcode_textarea_dimensions(&editor_textarea_ui_element)
-        {
-            if let Some(bounds_of_selected_text) =
-                get_bounds_of_first_char_in_range(&selected_text_range, &editor_textarea_ui_element)
+        // Get position of selected text
+        let mut scroll_delta = None;
+        if let Some(editor_textarea_ui_element) = get_textarea_uielement(pid) {
+            // Get the dimensions of the textarea viewport
+            if let Ok(textarea_viewport) =
+                derive_xcode_textarea_dimensions(&editor_textarea_ui_element)
             {
-                scroll_delta = Some(tauri::LogicalSize {
-                    width: textarea_viewport.0.x - bounds_of_selected_text.origin.x,
-                    height: bounds_of_selected_text.origin.y - textarea_viewport.0.y,
-                });
+                if let Some(bounds_of_selected_text) = get_bounds_of_first_char_in_range(
+                    &selected_text_range_move,
+                    &editor_textarea_ui_element,
+                ) {
+                    scroll_delta = Some(tauri::LogicalSize {
+                        width: textarea_viewport.0.x - bounds_of_selected_text.origin.x,
+                        height: bounds_of_selected_text.origin.y - textarea_viewport.0.y,
+                    });
+                }
             }
         }
-    }
 
-    // Update content
-    let formatted_content_string =
-        if let Ok(formatted_content_string) = String::from_utf16(&formatted_content) {
-            formatted_content_string
+        // Update content
+        let formatted_content_string =
+            if let Ok(formatted_content_string) = String::from_utf16(&new_text) {
+                formatted_content_string
+            } else {
+                return;
+            };
+
+        if let Ok(_) = update_xcode_editor_content(pid, &formatted_content_string) {
         } else {
-            return None;
+            return;
         };
-    if let Ok(_) = update_xcode_editor_content(pid, &formatted_content_string) {
-    } else {
-        return None;
-    };
 
-    // Restore cursor position
-    // At this point we only place the curser a the exact same ROW | COL as before the formatting.
-    if let Ok(_) = set_selected_text_range(
-        pid,
-        get_new_cursor_index(&old_text, &formatted_content, selected_text_range.index),
-        selected_text_range.length,
-    ) {}
+        // Restore cursor position
+        // At this point we only place the curser a the exact same ROW | COL as before the formatting.
+        if let Ok(_) = set_selected_text_range(
+            pid,
+            get_new_cursor_index(&old_text_move, &new_text, selected_text_range_move.index),
+            selected_text_range_move.length,
+        ) {}
 
-    // Scroll to the same position as before the formatting
-    let pid_move_copy = pid;
-    if let Some(scroll_delta) = scroll_delta {
-        tauri::async_runtime::spawn(async move {
-            tokio::time::sleep(std::time::Duration::from_millis(20)).await;
-            if let Ok(true) = send_event_mouse_wheel(pid_move_copy, scroll_delta) {}
-        });
-    }
+        // Scroll to the same position as before the formatting
+        if let Some(scroll_delta) = scroll_delta {
+            tauri::async_runtime::spawn(async move {
+                tokio::time::sleep(std::time::Duration::from_millis(20)).await;
+                if let Ok(true) = send_event_mouse_wheel(pid, scroll_delta) {}
+            });
+        }
 
-    // Notifiy the frontend that the file has been formatted successfully
-    EventRuleExecutionState::SwiftFormatFinished().publish_to_tauri(&app_handle);
-
-    Some(formatted_content)
+        // Notifiy the frontend that the file has been formatted successfully
+        EventRuleExecutionState::SwiftFormatFinished().publish_to_tauri(&app_handle_move);
+    });
 }
 
-pub fn format_swift_file(file_path: &String) -> Option<XcodeText> {
+async fn format_file(file_path: &String) -> Option<String> {
     if !Path::new(file_path).exists() {
         println!("File does not exist: {}", file_path);
         return None;
     }
-    let handle = format_file(file_path);
-    let formatted_file = block_on(handle);
 
-    if let Some(content) = formatted_file {
-        Some(XcodeText::from_str(&content))
-    } else {
-        None
-    }
-}
-
-async fn format_file(file_path: &String) -> Option<String> {
     let (mut rx, _) = Command::new_sidecar("swiftformat")
         .expect("failed to create `my-sidecar` binary command")
         .args([
@@ -144,6 +137,7 @@ mod tests {
     use std::process::Command as StdCommand;
 
     use rand::Rng;
+    use tauri::async_runtime::block_on;
 
     use super::*;
 
@@ -201,12 +195,13 @@ mod tests {
     }
 
     #[test]
-    fn test_format_swift_file() {
+    fn file_not_exists() {
         let test_resources = FileSystemSetup::new();
 
         // Format non-existing file
-        let formatted_content =
-            format_swift_file(&test_resources.test_file_not_existing_str.clone());
-        assert!(formatted_content.is_none());
+        let file_path = test_resources.test_file_not_existing_str.clone();
+        let handle = format_file(&file_path);
+        let formatted_file = block_on(handle);
+        assert!(formatted_file.is_none());
     }
 }
