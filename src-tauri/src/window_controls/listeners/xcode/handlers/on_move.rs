4bbe4679d03c68ee40f5da00427cee4b37973ab3
Daniel Owens <daniel@codealpha.dev>
2022-08-26 11:57:35 +0200

    COD-220: Try lock on move window

--- src-tauri/src/window_controls/listeners/xcode/handlers/on_move.rs
+++ src-tauri/src/window_controls/listeners/xcode/handlers/on_move.rs
@@ -13,7 +13,8 @@ pub fn on_move_editor_window(
     moved_msg: &EditorWindowMovedMessage,
 ) -> Option<()> {
     let window_manager = &mut window_manager.lock();
-    let editor_window_list = &mut window_manager.editor_windows().lock();
+    // If deadlock, discard message. TODO: Rethink logic.
+    let editor_window_list = &mut window_manager.editor_windows().try_lock()?;
 
     let editor_window = editor_window_list.get_mut(&moved_msg.uielement_hash)?;
 
