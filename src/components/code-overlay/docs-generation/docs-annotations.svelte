574d62ceb71d9b5fe916c27b5a43fa5d564cd6a6
Daniel Owens <daniel@codealpha.dev>
2022-08-26 12:08:39 +0200

    COD-220: Move all code annotation logic to correct file

--- src/components/code-overlay/docs-generation/docs-annotations.svelte
+++ src/components/code-overlay/docs-generation/docs-annotations.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { listen } from '@tauri-apps/api/event';
 	import type { ChannelList } from '../../../../src-tauri/bindings/ChannelList';
+	import type { EventDocsGeneration } from '../../../../src-tauri/bindings/features/docs_generation/EventDocsGeneration';
 	import type { UpdateCodeAnnotationMessage } from '../../../../src-tauri/bindings/features/docs_generation/UpdateCodeAnnotationMessage';
 	import type { LogicalPosition } from '../../../../src-tauri/bindings/geometry/LogicalPosition';
 	import type { EventRuleExecutionState } from '../../../../src-tauri/bindings/rule_execution_state/EventRuleExecutionState';
@@ -8,16 +9,51 @@
 	import type { TrackingAreaClickedMessage } from '../../../../src-tauri/bindings/window_controls/TrackingAreaClickedMessage';
 	import type { TrackingAreaEnteredMessage } from '../../../../src-tauri/bindings/window_controls/TrackingAreaEnteredMessage';
 	import type { TrackingAreaExitedMessage } from '../../../../src-tauri/bindings/window_controls/TrackingAreaExitedMessage';
+	import { convert_global_frame_to_local_frame } from '../../../utils';
 	import AnnotationIcon from './annotation-icon.svelte';
 	import AnnotationLine from './annotation-line.svelte';
-
-	export let annotation: UpdateCodeAnnotationMessage;
-	export let code_overlay_position: LogicalPosition | null;
+	
+	
+	export let code_document_origin: LogicalPosition;
+	
+	type CodeAnnotation = UpdateCodeAnnotationMessage;
+	let annotation: CodeAnnotation | undefined;
 
 	let is_hovered = false;
 	let is_processing = false;
 
 	let processing_timeout = 15000; // ms
+
+
+	function mapUpdateCodeAnnotationPayloadToCodeAnnotation(payload: UpdateCodeAnnotationMessage): CodeAnnotation | undefined {
+		return {
+			id: payload.id,
+			annotation_icon: payload.annotation_icon ? convert_global_frame_to_local_frame(payload.annotation_icon, code_document_origin) : null,
+			annotation_codeblock: payload.annotation_codeblock ? convert_global_frame_to_local_frame(payload.annotation_codeblock, code_document_origin) : null
+		}
+	
+	}
+	const listenToCodeAnnotationEvents = async () => {
+		// Listener for docs generation feature
+		let DocsGenerationChannel: ChannelList = 'EventDocsGeneration';
+		await listen(DocsGenerationChannel, (event) => {
+			const {payload, event: event_type} = JSON.parse(event.payload as string) as EventDocsGeneration;
+			switch (event_type) {
+				case 'UpdateCodeAnnotation':
+					annotation = mapUpdateCodeAnnotationPayloadToCodeAnnotation(payload);
+					break;
+				case 'RemoveCodeAnnotation':
+					if (annotation?.id === payload.id) {
+						annotation = undefined;
+					}
+					break;
+				default:
+					break;
+			}
+		});
+	};
+
+	listenToCodeAnnotationEvents();
 
 	const listenToDocsGenerationEvents = async () => {
 		// Listen for rule execution events to determine if the processing icon should be displayed
@@ -39,6 +75,7 @@
 			}
 		});
 	};
+	listenToDocsGenerationEvents();
 
 	const listenToTrackingAreaEvents = async () => {
 		// Listen for click & hover events on the tracking area
@@ -48,19 +85,19 @@
 			switch (tracking_area_event.event) {
 				case 'TrackingAreaClicked':
 					let clicked_msg = tracking_area_event.payload as unknown as TrackingAreaClickedMessage;
-					if (clicked_msg.id === annotation.id) {
+					if (clicked_msg.id === annotation?.id) {
 						is_hovered = true;
 					}
 					break;
 				case 'TrackingAreaEntered':
 					let entered_msg = tracking_area_event.payload as unknown as TrackingAreaEnteredMessage;
-					if (entered_msg.id === annotation.id) {
+					if (entered_msg.id === annotation?.id) {
 						is_hovered = true;
 					}
 					break;
 				case 'TrackingAreaExited':
 					let exited_msg = tracking_area_event.payload as unknown as TrackingAreaExitedMessage;
-					if (exited_msg.id === annotation.id) {
+					if (exited_msg.id === annotation?.id) {
 						is_hovered = false;
 					}
 					break;
@@ -71,37 +108,37 @@
 	};
 
 	listenToTrackingAreaEvents();
-	listenToDocsGenerationEvents();
 
 	const round_value = (value: number, precision: number): number => {
 		const factor = Math.pow(10, precision || 0);
 		return Math.round(value * factor) / factor;
 	};
 </script>
+{#if annotation}
+	{#if annotation.annotation_icon !== null}
+		<div
+			style="position: absolute; 
+			top: {round_value(annotation.annotation_icon.origin.y, 2)}px; 
+			left: {round_value(annotation.annotation_icon.origin.x, 2)}px; 
+			width: {round_value(annotation.annotation_icon.size.width, 2)}px; 
+			height: {round_value(annotation.annotation_icon.size.height, 2)}px;"
+		>
+			<AnnotationIcon {is_hovered} {is_processing} />
+		</div>
+	{/if}
 
-{#if code_overlay_position !== null && annotation.annotation_icon !== null}
-	<div
-		style="position: absolute; 
-		top: {round_value(annotation.annotation_icon.origin.y, 2)}px; 
-		left: {round_value(annotation.annotation_icon.origin.x, 2)}px; 
-		width: {round_value(annotation.annotation_icon.size.width, 2)}px; 
-		height: {round_value(annotation.annotation_icon.size.height, 2)}px;"
-	>
-		<AnnotationIcon {is_hovered} {is_processing} />
-	</div>
-{/if}
-
-{#if code_overlay_position !== null && annotation.annotation_codeblock !== null}
-	<div
-		style="position: absolute; 
-		top: {round_value(annotation.annotation_codeblock.origin.y, 2)}px; 
-		left: {round_value(annotation.annotation_codeblock.origin.x, 2)}px; 
-		width: {round_value(annotation.annotation_codeblock.size.width, 2)}px; 
-		height: {round_value(annotation.annotation_codeblock.size.height, 2)}px;"
-	>
-		<AnnotationLine
-			visible={is_hovered || is_processing}
-			highlighted={is_hovered && !is_processing}
-		/>
-	</div>
+	{#if annotation.annotation_codeblock !== null}
+		<div
+			style="position: absolute; 
+			top: {round_value(annotation.annotation_codeblock.origin.y, 2)}px; 
+			left: {round_value(annotation.annotation_codeblock.origin.x, 2)}px; 
+			width: {round_value(annotation.annotation_codeblock.size.width, 2)}px; 
+			height: {round_value(annotation.annotation_codeblock.size.height, 2)}px;"
+		>
+			<AnnotationLine
+				visible={is_hovered || is_processing}
+				highlighted={is_hovered && !is_processing}
+			/>
+		</div>
+	{/if}
 {/if}
--- src/pages/code-overlay.svelte
+++ src/pages/code-overlay.svelte
@@ -1,24 +1,18 @@
 <script lang="ts">
 	import { listen } from '@tauri-apps/api/event';
 	import type { ChannelList } from '../../src-tauri/bindings/ChannelList';
-	import type { EventDocsGeneration } from '../../src-tauri/bindings/features/docs_generation/EventDocsGeneration';
 	import type { LogicalFrame } from '../../src-tauri/bindings/geometry/LogicalFrame';
 	import type { EventWindowControls } from '../../src-tauri/bindings/window_controls/EventWindowControls';
 	import BracketHighlight from '../components/code-overlay/bracket-highlight/bracket-highlight.svelte';
 	import DocsAnnotations from '../components/code-overlay/docs-generation/docs-annotations.svelte';
 
 	import { getContext } from 'svelte';
-	import type { UpdateCodeAnnotationMessage } from '../../src-tauri/bindings/features/docs_generation/UpdateCodeAnnotationMessage';
-	import { convert_global_frame_to_local_frame } from '../utils';
 	
 	const { setTheme } = getContext("theme");
 
-	type CodeAnnotation = UpdateCodeAnnotationMessage;
 
 	let code_viewport_rectangle: LogicalFrame | null = null;
 	let code_document_rectangle: LogicalFrame | null = null;
-
-	let code_annotation: CodeAnnotation | undefined = undefined;
 
 
 	const listenWindowChannels = async () => {
@@ -43,38 +37,6 @@
 		});
 	}
 
-	function mapUpdateCodeAnnotationPayloadToCodeAnnotation(payload: UpdateCodeAnnotationMessage): CodeAnnotation | undefined {
-		if (!code_document_rectangle) {
-			return undefined;
-		}
-		return {
-			id: payload.id,
-			annotation_icon: payload.annotation_icon ? convert_global_frame_to_local_frame(payload.annotation_icon, code_document_rectangle) : null,
-			annotation_codeblock: payload.annotation_codeblock ? convert_global_frame_to_local_frame(payload.annotation_codeblock, code_document_rectangle) : null
-		}
-	
-	}
-	const listenCodeAnnotations = async () => {
-		// Listener for docs generation feature
-		let DocsGenerationChannel: ChannelList = 'EventDocsGeneration';
-		await listen(DocsGenerationChannel, (event) => {
-			const {payload, event: event_type} = JSON.parse(event.payload as string) as EventDocsGeneration;
-			switch (event_type) {
-				case 'UpdateCodeAnnotation':
-					code_annotation = mapUpdateCodeAnnotationPayloadToCodeAnnotation(payload);
-					break;
-				case 'RemoveCodeAnnotation':
-					if (code_annotation?.id === payload.id) {
-						code_annotation = undefined;
-					}
-					break;
-				default:
-					break;
-			}
-		});
-	};
-
-	listenCodeAnnotations();
 	listenWindowChannels();
 
 </script>
@@ -95,12 +57,9 @@
 			class="h-full w-full overflow-hidden relative"
 		>
 			<BracketHighlight code_document_rectangle={code_document_rectangle} />
-			{#if code_annotation}
-				<DocsAnnotations
-					annotation={code_annotation}
-					code_overlay_position={code_document_rectangle.origin}
-				/>
-			{/if}
+			<DocsAnnotations
+				code_document_origin={code_document_rectangle.origin}
+			/>
 		</div>
 	</div>
 {/if}
