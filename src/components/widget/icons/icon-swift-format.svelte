2621e337a1ba1e768a0795ba859297da7c96ceaf
Adrian Hupka <adrian@hupka.ventures>
2022-08-28 16:12:12 +0200

    COD-153: Slightly adjusted the shake animation to be more subtle.

--- src/components/widget/icons/icon-swift-format.svelte
+++ src/components/widget/icons/icon-swift-format.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+	export let fill: string;
+	export let blur: boolean;
+</script>
+
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 47 47">
-	<g fill="#E5E7EB" filter="url(#a)">
+	<g {fill} filter="url(#a)">
 		<path
 			d="M20.1 33c1.2.64 2.09.87 2.65.7.57-.18 1.18-.88 1.84-2.11l1.44-2.7c.79-1.47 1.64-2.32 2.56-2.55.92-.24 2.27-.01 4.05.68-1.6-1.15-2.56-2.14-2.87-2.98-.32-.84-.07-2.02.74-3.53l1.85-3.47c.65-1.22.9-2.12.72-2.68-.16-.58-.84-1.18-2.05-1.83l-.9-.48.99-1.85.82.44c2.15 1.15 3.41 2.23 3.8 3.26.4 1.02.08 2.48-.94 4.38L33 21.64a4.54 4.54 0 0 0-.64 2.93c.17.83.87 1.57 2.08 2.22l1.58.84-.99 1.85-1.58-.84c-1.1-.6-2.11-.7-3-.3a4.37 4.37 0 0 0-2.1 2l-1.38 2.6c-1.01 1.9-2.06 2.97-3.13 3.22-1.06.25-2.67-.2-4.81-1.35l-.83-.44 1-1.86.9.48Z"
 		/>
@@ -21,10 +26,15 @@
 				result="hardAlpha"
 				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
 			/>
+			{#if blur}
+				<feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.8 0" />
+			{:else}
+				<feColorMatrix values="0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0.8 0" />
+			{/if}
+
 			<feOffset />
 			<feGaussianBlur stdDeviation="1.5" />
 			<feComposite in2="hardAlpha" operator="out" />
-			<feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.8 0" />
 			<feBlend in2="BackgroundImageFix" result="effect1_dropShadow_865_5324" />
 			<feBlend in="SourceGraphic" in2="effect1_dropShadow_865_5324" result="shape" />
 		</filter>
