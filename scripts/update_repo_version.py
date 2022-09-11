4d9e13ccf0567b3419358191a633de69becae0f6
Adam Nyberg <adam.nyberg@gmail.com>
2022-09-11 22:46:57 +0200

    COD-72: Fix release

--- scripts/update_repo_version.py
+++ scripts/update_repo_version.py
@@ -2,12 +2,14 @@ import json
 import os
 import toml
 
+print("Updating repo version to ", os.environ["VERSION"])
+
 package_file_path = "./package.json"
 with open(package_file_path, "r") as jsonFile:
     data = json.load(jsonFile)
 data["version"] = os.environ['VERSION']
 with open(package_file_path, "w") as jsonFile:
-    json.dump(data, jsonFile, indent=2)
+    jsonFile.write(json.dumps(data, indent=2) + '\n')
 print('Updated package.json version to ' + os.environ['VERSION'])
 
 tauri_conf_file_path = "./src-tauri/tauri.conf.json"
@@ -15,13 +17,9 @@ with open(tauri_conf_file_path, "r") as jsonFile:
     data = json.load(jsonFile)
 data["package"]["version"] = os.environ['VERSION']
 with open(tauri_conf_file_path, "w") as jsonFile:
-    json.dump(data, jsonFile, indent=2)
-print('Updated tauri.conf.json version to ' + os.environ['VERSION'])
-
+    jsonFile.write(json.dumps(data, indent=2) + '\n')
 cargo_toml_file_path = "./src-tauri/Cargo.toml"
-data = toml.load(cargo_toml_file_path)
 data['package']['version'] = os.environ['VERSION']
-f = open(cargo_toml_file_path, 'w')
-toml.dump(data, f)
-f.close()
+with open(cargo_toml_file_path, "w") as tomlFile:
+    tomlFile.write(toml.dumps(data) + '\n')
 print('Updated Cargo.toml version to ' + os.environ['VERSION'])
