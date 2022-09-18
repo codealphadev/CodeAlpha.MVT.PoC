cb2473838db699ad08909168bac8328291fb1b70
Daniel Owens <daniel@codealpha.dev>
2022-09-18 16:51:14 +0200

    COD-317: Fix css bug for gradient text

--- src/components/node-explainer/icons/icon-thumbs-down.svelte
+++ src/components/node-explainer/icons/icon-thumbs-down.svelte
@@ -10,22 +10,14 @@
 
 <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M12.6666 15.3333H9.49069C8.49952 15.3333 7.85486 14.2902 8.29812 13.4037L10.6315 8.73705C10.8573 8.28534 11.319 8 11.824 8H14.5025C14.6115 8 14.7201 8.01337 14.8259 8.03981L17.3333 8.66667M12.6666 15.3333V18.6667C12.6666 19.403 13.2636 20 14 20H14.0636C14.3967 20 14.6666 19.73 14.6666 19.397C14.6666 18.9208 14.8076 18.4552 15.0718 18.059L17.3333 14.6667V8.66667M12.6666 15.3333H14M17.3333 8.66667H18.6666C19.403 8.66667 20 9.26362 20 10V14C20 14.7364 19.403 15.3333 18.6666 15.3333H17" stroke="url(#paint0_{theme_name})" stroke-width="1.33" stroke-linecap="round" stroke-linejoin="round"/>
-    <rect x="0.75" y="0.75" width="26.5" height="26.5" rx="13.25" stroke="url(#paint1_{theme_name})" stroke-width="1.5"/>
+    <rect x="0.75" y="0.75" width="26.5" height="26.5" rx="13.25" stroke="url(#paint0_{theme_name})" stroke-width="1.5"/>
     <defs>
-    <linearGradient id="paint0_dark" x1="20" y1="8" x2="8.001" y2="19.8433" gradientUnits="userSpaceOnUse">
-    <stop stop-color="##FF9C64"/>
-    <stop offset=".782" stop-color="#F84545"/>
-    </linearGradient>
-    <linearGradient id="paint1_dark" x1="28" y1="-1.66893e-06" x2="1.66893e-06" y2="28" gradientUnits="userSpaceOnUse">
+    <linearGradient id="paint0_dark" x1="28" y1="-1.66893e-06" x2="1.66893e-06" y2="28" gradientUnits="userSpaceOnUse">
     <stop stop-color="#FF9C64"/>
     <stop offset=".782" stop-color="#F84545"/>
     </linearGradient>
 
-    <linearGradient id="paint0_light" x1="20" y1="8" x2="8.001" y2="19.8433" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#0B9CDA"/>
-        <stop offset="1" stop-color="#054B8B"/>
-        </linearGradient>
-        <linearGradient id="paint1_light" x1="28" y1="-1.66893e-06" x2="1.66893e-06" y2="28" gradientUnits="userSpaceOnUse">
+        <linearGradient id="paint0_light" x1="28" y1="-1.66893e-06" x2="1.66893e-06" y2="28" gradientUnits="userSpaceOnUse">
         <stop stop-color="#0B9CDA"/>
         <stop offset="1" stop-color="#054B8B"/>
         </linearGradient>
