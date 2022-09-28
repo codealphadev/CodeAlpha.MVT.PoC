f0b65d84eecf403df91dd5f9f39b0462f2b3f400
Daniel Owens <daniel@codealpha.dev>
2022-09-28 15:27:03 +0200

    COD-340: Use SourceKit refactoring

--- src-tauri/src/core_engine/features/complexity_refactoring/mod.rs
+++ src-tauri/src/core_engine/features/complexity_refactoring/mod.rs
@@ -2,7 +2,8 @@ pub use complexity_refactoring::ComplexityRefactoring;
 pub use complexity_refactoring::ComplexityRefactoringError;
 pub use method_extraction::check_for_method_extraction;
 pub use swift_lsp::*;
-
+pub use utils::*;
 mod complexity_refactoring;
 mod method_extraction;
 mod swift_lsp;
+mod utils;
