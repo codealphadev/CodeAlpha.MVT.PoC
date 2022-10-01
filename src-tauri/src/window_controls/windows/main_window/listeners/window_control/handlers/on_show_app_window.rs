da6fb6199262f4b04b6ab4fc53227f3b4d0119a1
Adrian Hupka <adrian@hupka.ventures>
2022-10-01 17:00:43 +0200

    COD-377: Added behavior for multiscreen setups.

--- src-tauri/src/window_controls/windows/main_window/listeners/window_control/handlers/on_show_app_window.rs
+++ src-tauri/src/window_controls/windows/main_window/listeners/window_control/handlers/on_show_app_window.rs
@@ -1,10 +1,15 @@
 use std::sync::Arc;
 
 use parking_lot::Mutex;
+use tauri::Manager;
 use tracing::debug;
 
-use crate::window_controls::{
-    config::AppWindow, events::models::app_window::ShowAppWindowMessage, windows::MainWindow,
+use crate::{
+    app_handle,
+    utils::geometry::{LogicalFrame, LogicalPosition, LogicalSize},
+    window_controls::{
+        config::AppWindow, events::models::app_window::ShowAppWindowMessage, windows::MainWindow,
+    },
 };
 
 pub fn on_show_app_window(
@@ -14,7 +19,18 @@ pub fn on_show_app_window(
     if show_msg.app_windows.contains(&AppWindow::Main) {
         let main_window = main_window.lock();
 
-        if main_window.show(&show_msg.monitor).is_none() {
+        // We fetch the window where the widget is on
+        let widget_window = app_handle().get_window(&AppWindow::Widget.to_string())?;
+        let monitor = widget_window.current_monitor().ok()??;
+
+        let scale_factor = monitor.scale_factor();
+        let origin = LogicalPosition::from_tauri_LogicalPosition(
+            &monitor.position().to_logical::<f64>(scale_factor),
+        );
+        let size =
+            LogicalSize::from_tauri_LogicalSize(&monitor.size().to_logical::<f64>(scale_factor));
+
+        if main_window.show(&LogicalFrame { origin, size }).is_none() {
             debug!("Failed to show MainWindow (on_show_app_window.rs)");
         }
     }
