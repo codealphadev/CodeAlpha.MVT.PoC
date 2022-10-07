46372ebc4c7d6eb1da09d8115f10f26de70a0a49
Daniel Owens <daniel@codealpha.dev>
2022-10-07 13:25:08 +0200

    COD-392: Add state for no suggestions found

--- src/components/common/card.svelte
+++ src/components/common/card.svelte
@@ -2,11 +2,12 @@
 	import { fade } from 'svelte/transition';
 
 	export let id: string | undefined = undefined;
+	export let additional_class: string = '';
 </script>
 
 <div
 	{id}
-	class="rounded-xl bg-background overflow-hidden py-4 px-5 flex flex-col items-start gap-3 border-none"
+	class="rounded-xl bg-background overflow-hidden py-4 px-5 flex flex-col items-start gap-3 border-none {additional_class}"
 	in:fade={{ duration: 100 }}
 >
 	<slot />
---
+++ src/components/suggestions/no-suggestions.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Card from '../common/card.svelte';
+	import Caption from '../common/typography/caption.svelte';
+	import H2 from '../common/typography/h2.svelte';
+	import NoSuggestionsFound from './icons/no-suggestions-found.svelte';
+</script>
+
+<Card additional_class="items-center justify-center h-36">
+	<NoSuggestionsFound />
+
+	<div class="flex flex-col items-center justify-center gap-1">
+		<H2>Pretty clean here!</H2>
+		<Caption>We couldn't spot anything. Happy coding!</Caption>
+	</div>
+</Card>
