32dc106da916069c5a2597cacedf3957a98b2282
Adrian Hupka <adrian@hupka.ventures>
2022-10-08 00:36:36 +0200

    COD-406: Added granular mouse events to TAM.

--- src-tauri/src/window_controls/events/models/tracking_area/tracking_area_clicked_outside.rs
+++ src-tauri/src/window_controls/events/models/tracking_area/tracking_area_clicked_outside.rs
@@ -1,7 +1,12 @@
 use serde::{Deserialize, Serialize};
 use ts_rs::TS;
 
-use crate::{core_engine::EditorWindowUid, window_controls::config::AppWindow};
+use crate::{
+    core_engine::EditorWindowUid,
+    platform::macos::models::input_device::{ClickType, MouseButton},
+    utils::geometry::LogicalPosition,
+    window_controls::config::AppWindow,
+};
 
 #[derive(Clone, Debug, Serialize, Deserialize, TS)]
 #[ts(export, export_to = "bindings/window_controls/")]
@@ -9,4 +14,7 @@ pub struct TrackingAreaClickedOutsideMessage {
     pub id: uuid::Uuid,
     pub window_uid: EditorWindowUid,
     pub app_window: AppWindow,
+    pub mouse_position: LogicalPosition,
+    pub button: MouseButton,
+    pub click_type: ClickType,
 }
