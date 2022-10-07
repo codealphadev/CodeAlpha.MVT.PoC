81dd5f4769daa43c6d077572d862ddc748db1e16
Adrian Hupka <adrian@hupka.ventures>
2022-10-07 14:17:58 +0200

    COD-406: Part 1 BracketHighlight Refactoring: Frontend Logic

--- src/components/code-overlay/bracket-highlight/bracket-highlight.svelte
+++ src/components/code-overlay/bracket-highlight/bracket-highlight.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
 	import { listen } from '@tauri-apps/api/event';
+	import type { LogicalPosition } from '@tauri-apps/api/window';
 	import type { ChannelList } from '../../../../src-tauri/bindings/ChannelList';
 	import type { BracketHighlightResults } from '../../../../src-tauri/bindings/features/bracket_highlighting/BracketHighlightResults';
+	import type { AnnotationGroup } from '../../../../src-tauri/bindings/features/code_annotations/AnnotationGroup';
+	import type { AnnotationShape } from '../../../../src-tauri/bindings/features/code_annotations/AnnotationShape';
+	import type { AnnotationEvent } from '../../../../src-tauri/bindings/features/node_annotation/AnnotationEvent';
 	import type { LogicalFrame } from '../../../../src-tauri/bindings/geometry/LogicalFrame';
 	import { colors } from '../../../themes';
-	import { BORDER_WIDTH, compute_bracket_highlight_rects } from './bracket_highlight';
+	import {
+		BORDER_WIDTH,
+		compute_bracket_highlight_lines,
+		compute_bracket_highlight_rects
+	} from './bracket_highlight';
 
 	export let code_document_rect: LogicalFrame;
 	export let active_window_uid: number;
+
+	let annotation_group: AnnotationGroup | undefined;
 
 	let top_rectangle: LogicalFrame | null = null;
 	let bottom_rectangle: LogicalFrame | null = null;
@@ -43,6 +53,152 @@
 		});
 	};
 	listenTauriEvents();
+
+	const listen_to_node_annotation_events = async () => {
+		let node_annotation_channel: ChannelList = 'NodeAnnotationEvent';
+		await listen(node_annotation_channel, (event) => {
+			const { payload, event: event_type } = JSON.parse(event.payload as string) as AnnotationEvent;
+			switch (event_type) {
+				case 'AddAnnotationGroup':
+					let added_group = payload as AnnotationGroup;
+
+					if (added_group.feature === 'BracketHighlight') {
+						annotation_group = added_group;
+
+						update_brackets();
+						update_rectangles();
+
+						console.log('AddAnnotationGroup', payload);
+					}
+
+					break;
+				case 'UpdateAnnotationGroup':
+					let updated_group = payload as AnnotationGroup;
+
+					if (updated_group.feature === 'BracketHighlight') {
+						annotation_group = updated_group;
+
+						update_brackets();
+						update_rectangles();
+
+						console.log('UpdateAnnotationGroup', payload);
+					}
+
+					break;
+				case 'RemoveAnnotationGroup':
+					let group_id = payload as string;
+
+					if (annotation_group?.id === group_id) {
+						annotation_group = undefined;
+						opening_bracket_box = null;
+						closing_bracket_box = null;
+						top_rectangle = null;
+						bottom_rectangle = null;
+					}
+
+					console.log('RemoveAnnotationGroup', payload);
+					break;
+				default:
+					break;
+			}
+		});
+	};
+	listen_to_node_annotation_events();
+
+	const update_brackets = async () => {
+		opening_bracket_box = null;
+		closing_bracket_box = null;
+
+		if (annotation_group == null) {
+			return;
+		}
+
+		let bracket_open = annotation_group.annotations.find(
+			(annotation) => annotation.kind === 'ClosingBracket'
+		);
+
+		let bracket_close = annotation_group.annotations.find(
+			(annotation) => annotation.kind === 'OpeningBracket'
+		);
+
+		if (!bracket_open || !bracket_close) {
+			return;
+		}
+
+		if (bracket_open.shapes[0] === undefined || bracket_close.shapes[0] === undefined) {
+			return;
+		}
+
+		if (!is_rectangle(bracket_open.shapes[0]) || !is_rectangle(bracket_close.shapes[0])) {
+			return;
+		}
+
+		opening_bracket_box = bracket_open.shapes[0].Rectangle;
+		closing_bracket_box = bracket_close.shapes[0].Rectangle;
+	};
+
+	const update_rectangles = async () => {
+		top_rectangle = null;
+		bottom_rectangle = null;
+
+		let lines_start_pos = null;
+		let lines_end_pos = null;
+		let elbow_pos = null;
+
+		if (annotation_group == null) {
+			return;
+		}
+
+		let lines_start = annotation_group.annotations.find(
+			(annotation) => annotation.kind === 'LineStart'
+		);
+
+		let lines_end = annotation_group.annotations.find(
+			(annotation) => annotation.kind === 'LineEnd'
+		);
+
+		let elbow = annotation_group.annotations.find((annotation) => annotation.kind === 'Elbow');
+
+		if (!lines_start || !lines_end || !elbow) {
+			return;
+		}
+
+		if (lines_start.shapes[0] !== undefined) {
+			if (is_point(lines_start.shapes[0])) {
+				lines_start_pos = lines_start.shapes[0].Point;
+			}
+		}
+
+		if (lines_end.shapes[0] !== undefined) {
+			if (is_point(lines_end.shapes[0])) {
+				lines_end_pos = lines_end.shapes[0].Point;
+			}
+		}
+
+		if (elbow.shapes[0] !== undefined) {
+			if (is_point(elbow.shapes[0])) {
+				elbow_pos = elbow.shapes[0].Point;
+			}
+		}
+
+		const rectangles = compute_bracket_highlight_lines(
+			lines_start_pos,
+			lines_end_pos,
+			elbow_pos?.x,
+			code_document_rect.size.height
+		);
+
+		top_rectangle = rectangles.top_rect;
+		bottom_rectangle = rectangles.bottom_rect;
+	};
+
+	function is_rectangle(shape: AnnotationShape): shape is { Rectangle: LogicalFrame } {
+		return shape.hasOwnProperty('Rectangle');
+	}
+
+	function is_point(shape: AnnotationShape): shape is { Point: LogicalPosition } {
+		return shape.hasOwnProperty('Point');
+	}
 
 	const round_value = (value: number, precision: number): number => {
 		const factor = Math.pow(10, precision || 0);
