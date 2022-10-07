2e0250923c847c89c513fb484a69edbcaaab03d7
Daniel Owens <daniel@codealpha.dev>
2022-10-07 15:19:13 +0200

    COD-402: PR comments

--- src/components/suggestions/no-suggestions.svelte
+++ src/components/suggestions/no-suggestions.svelte
@@ -2,11 +2,11 @@
 	import Card from '../common/card.svelte';
 	import Caption from '../common/typography/caption.svelte';
 	import H2 from '../common/typography/h2.svelte';
-	import NoSuggestionsFound from './icons/no-suggestions-found.svelte';
+	import NoSuggestionsFoundIcon from './icons/no-suggestions-found.svelte';
 </script>
 
 <Card additional_class="items-center justify-center h-36">
-	<NoSuggestionsFound />
+	<NoSuggestionsFoundIcon />
 
 	<div class="flex flex-col items-center justify-center gap-1">
 		<H2>Pretty clean here!</H2>
