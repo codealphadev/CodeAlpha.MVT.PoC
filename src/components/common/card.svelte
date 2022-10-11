b1bc7f05822dca46c507270071cff4d42b278f41
Adrian Hupka <adrian@hupka.ventures>
2022-10-11 11:51:21 +0200

    COD-394: Fixing shadows for Explain window; using theme background color.

--- src/components/common/card.svelte
+++ src/components/common/card.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
-
 	export let id: string | undefined = undefined;
 	export let additional_class: string = '';
 </script>
@@ -8,7 +6,6 @@
 <div
 	{id}
 	class="rounded-xl bg-background overflow-hidden py-4 px-5 flex flex-col items-start gap-3 border-none shrink-0 {additional_class}"
-	in:fade={{ duration: 100 }}
 >
 	<slot />
 </div>
