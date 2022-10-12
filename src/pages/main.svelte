a93b87643724d4803bf39d6d3f05ecb4741f87c9
Adrian Hupka <adrian@hupka.ventures>
2022-10-12 12:02:40 +0200

    COD-437: Added shared Enum for TailOrientation

--- src/pages/main.svelte
+++ src/pages/main.svelte
@@ -2,24 +2,21 @@
 	import { Event, listen } from '@tauri-apps/api/event';
 	import Tail from '../components/main/tail.svelte';
 	import Suggestions from '../components/suggestions/suggestions.svelte';
+	import type { TailOrientation } from '../../src-tauri/bindings/window_controls/TailOrientation';
 
 	let dom_id = 'main-window-container';
 
-	// Enum for the orientation of the tail
-	enum TailOrientation {
-		Left = 'left',
-		Right = 'right'
-	}
-
-	let tail_orientation = TailOrientation.Right;
+	let tail_orientation: TailOrientation = 'Right';
 	const listenToGlobalEvents = async () => {
 		await listen('tail-orientation-flipped', (event) => {
 			const tauriEvent = event as Event<any>;
 
-			if (tauriEvent.payload) {
-				tail_orientation = TailOrientation.Left;
+			console.log('tauriEvent', tauriEvent.payload);
+
+			if ((tauriEvent.payload as TailOrientation) == 'Left') {
+				tail_orientation = 'Left';
 			} else {
-				tail_orientation = TailOrientation.Right;
+				tail_orientation = 'Right';
 			}
 		});
 	};
@@ -34,11 +31,7 @@
 	>
 		<Suggestions window_dom_id={dom_id} />
 	</div>
-	<div
-		class="h-3  {`${
-			tail_orientation == TailOrientation.Left ? 'mr-auto ml-[18px]' : 'ml-auto mr-[18px]'
-		}`}"
-	>
+	<div class="h-3  {`${tail_orientation == 'Left' ? 'mr-auto ml-[18px]' : 'ml-auto mr-[18px]'}`}">
 		<Tail />
 	</div>
 </div>
