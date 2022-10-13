b4fbab233e53adba7790da8ef5e2aa7bac2ee67b
Daniel Owens <daniel@codealpha.dev>
2022-10-13 12:45:42 +0200

    COD-446: Frontend for clicking on suggestion

--- src-tauri/src/core_engine/events/user_interaction.rs
+++ src-tauri/src/core_engine/events/user_interaction.rs
@@ -1,17 +1,16 @@
 use serde::{Deserialize, Serialize};
 use ts_rs::TS;
 
-use super::models::{
-    CoreActivationStatusMessage, NodeAnnotationClickedMessage, RefactoringMessage,
-};
+use super::models::{CoreActivationStatusMessage, NodeAnnotationClickedMessage, SuggestionMessage};
 
 #[derive(Clone, Serialize, Deserialize, Debug, TS)]
 #[ts(export, export_to = "bindings/user_interaction/")]
 #[serde(tag = "event", content = "payload")]
 pub enum EventUserInteraction {
     CoreActivationStatus(CoreActivationStatusMessage),
-    PerformRefactoringOperation(RefactoringMessage),
-    DismissRefactoringSuggestion(RefactoringMessage),
+    PerformRefactoringOperation(SuggestionMessage),
+    DismissSuggestion(SuggestionMessage),
+    SelectSuggestion(SuggestionMessage),
     ToggleMainWindow(bool),
     NodeAnnotationClicked(NodeAnnotationClickedMessage),
 }
--- src/components/suggestions/suggestion.svelte
+++ src/components/suggestions/suggestion.svelte
@@ -12,44 +12,51 @@
   import Button from '../common/button/button.svelte';
   import { ButtonType } from '../common/button/button';
 
-  export let suggestion: FERefactoringSuggestion;
-  export let suggestion_id: string;
-  export let window_uid: number;
+	export let suggestion: FERefactoringSuggestion;
+	export let suggestion_id: string;
+	export let window_uid: number;
+	export let expanded = false;
 
-  const apply_suggestion = async () => {
-    const event: EventUserInteraction = {
-      event: 'PerformRefactoringOperation',
-      payload: { id: suggestion_id, editor_window_uid: window_uid },
-    };
-    const channel: ChannelList = 'EventUserInteractions';
+	const apply_suggestion = async () => {
+		const event: EventUserInteraction = {
+			event: 'PerformRefactoringOperation',
+			payload: { id: suggestion_id, editor_window_uid: window_uid }
+		};
+		const channel: ChannelList = 'EventUserInteractions';
 
-    await emit(channel, event);
-  };
+		await emit(channel, event);
+	};
 
-  const dismiss_suggestion = async () => {
-    const event: EventUserInteraction = {
-      event: 'DismissRefactoringSuggestion',
-      payload: { id: suggestion_id, editor_window_uid: window_uid },
-    };
-    const channel: ChannelList = 'EventUserInteractions';
-    await emit(channel, event);
-  };
+	const dismiss_suggestion = async () => {
+		const event: EventUserInteraction = {
+			event: 'DismissSuggestion',
+			payload: { id: suggestion_id, editor_window_uid: window_uid }
+		};
+		const channel: ChannelList = 'EventUserInteractions';
+		await emit(channel, event);
+	};
 </script>
 
-<Card>
-  <header>
-    <H3>Reduce complexity</H3>
-    <P>
-      Your function <code>{suggestion.main_function_name}</code> may be hard to understand due to nested
-      statements. Consider extracting this code block into a separate function.
-    </P>
-  </header>
-  <H4>Impact on complexity</H4>
-  <div class="flex items-center gap-1">
-    <ComplexityBadge complexity={suggestion.prev_complexity} />
-    <Arrow />
-    <ComplexityBadge complexity={suggestion.new_complexity} />
-  </div>
+<Card on:click>
+	<header>
+		<H3>Reduce complexity</H3>
+		<p class="text-contrast text-sm leading-[1.714]">
+			{#if expanded}
+				Your function <code>{suggestion.main_function_name}</code> may be hard to understand due to nested
+				statements. Consider extracting this code block into a separate function.
+			{:else}
+				Refactor deeply nested statements in function <code>{suggestion.main_function_name}</code>
+			{/if}
+		</p>
+	</header>
+	{#if expanded}
+		<div class="flex flex-col shrink-0 gap-3 items-start">
+			<H4>Impact on complexity</H4>
+			<div class="flex items-center gap-1">
+				<ComplexityBadge complexity={suggestion.prev_complexity} />
+				<Arrow />
+				<ComplexityBadge complexity={suggestion.new_complexity} />
+			</div>
 
   <div class="flex justify-between w-full items-center">
     <Button type={ButtonType.Primary} on:click={apply_suggestion}>Extract function</Button>
