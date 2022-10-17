7d108dfce0963f3364a598c3e58a9c9368d7d53f
Daniel Owens <daniel@codealpha.dev>
2022-10-17 10:45:57 +0200

    COD-468: Make widget text unselectable

--- src/components/widget/suggestions-number.svelte
+++ src/components/widget/suggestions-number.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-  export let count: number;
+	export let count: number;
 </script>
 
-<div class="text-primary w-full h-full flex justify-center items-center font-medium text-xl">
-  {count}
+<div
+	class="text-primary select-none w-full h-full flex justify-center items-center font-medium text-xl"
+>
+	{count}
 </div>
