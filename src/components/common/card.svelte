c2874d9ed53c4f2a6cff33afd402750ee86be497
Adam Nyberg <adam.nyberg@gmail.com>
2022-10-18 11:42:45 +0200

    COD-472: Fix paddings suggestion and add suggestion icon

--- src/components/common/card.svelte
+++ src/components/common/card.svelte
@@ -6,7 +6,7 @@
 <div
 	on:click
 	{id}
-	class="rounded-xl bg-background overflow-hidden py-4 px-5 flex flex-col items-start gap-3 border border-transparent shrink-0 {additional_class} shadow"
+	class="rounded-xl bg-background overflow-hidden py-4 px-4 flex flex-col items-start gap-3 border border-transparent shrink-0 {additional_class} shadow"
 >
 	<slot />
 </div>
