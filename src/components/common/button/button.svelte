f09614d09b4d3fb6bac81d970c8d7eb53b3f0b08
Adam Nyberg <adam.nyberg@gmail.com>
2022-10-19 11:00:56 +0200

    COD-493: Increase font weight of button

--- src/components/common/button/button.svelte
+++ src/components/common/button/button.svelte
@@ -10,7 +10,7 @@
 	class="
     inline-flex items-center justify-center rounded py-[7px] px-[11px] h-[30px] border-none text-xs
     {type === ButtonType.Primary
-		? 'button-primary bg-accent_primary shadow-[2px_2px_0px_#FD583A] text-white'
+		? 'button-primary bg-accent_primary shadow-[2px_2px_0px_#FD583A] text-white font-semibold'
 		: 'button-secondary bg-background text-[#374151]'}
     "
 >
