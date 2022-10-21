53988e202acb4b59a09523426065b1f0645ea466
Adrian Hupka <adrian@hupka.ventures>
2022-10-21 10:15:39 +0200

    COD-503: Fixed PR remarks.

--- src-tauri/src/core_engine/events/models/swift_format_cmd_s_activation_status.rs
+++ src-tauri/src/core_engine/events/models/swift_format_cmd_s_activation_status.rs
@@ -4,5 +4,5 @@ use ts_rs::TS;
 #[derive(Clone, Debug, Serialize, Deserialize, TS)]
 #[ts(export, export_to = "bindings/user_interaction/")]
 pub struct SwiftFormatOnCMDSMessage {
-    pub swift_format_on_cmd_s_active: bool,
+    pub active: bool,
 }
