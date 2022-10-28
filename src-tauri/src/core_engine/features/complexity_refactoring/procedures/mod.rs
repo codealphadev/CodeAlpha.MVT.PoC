ad7a9c3b66e4b4c57ac301908ae79c676a5c1e99
Daniel Owens <daniel@codealpha.dev>
2022-10-28 11:09:28 +0200

    COD-501: Refactor out compute_suggestions procedure

--- src-tauri/src/core_engine/features/complexity_refactoring/procedures/mod.rs
+++ src-tauri/src/core_engine/features/complexity_refactoring/procedures/mod.rs
@@ -1,7 +1,9 @@
+pub use compute_suggestions::compute_suggestions;
 pub use dismiss_suggestion::*;
 pub use perform_suggestion::perform_suggestion;
 pub use select_suggestion::select_suggestion;
 
+mod compute_suggestions;
 mod dismiss_suggestion;
 mod perform_suggestion;
 mod select_suggestion;
