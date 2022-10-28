df4cbe3fb3d9618cae57c3de07e3aec0f82081d3
Daniel Owens <daniel@codealpha.dev>
2022-10-28 11:51:09 +0200

    COD-501: Don't print error for cancellation case

--- src/components/suggestions/suggestions.ts
+++ src/components/suggestions/suggestions.ts
@@ -2,27 +2,27 @@ import type { FERefactoringSuggestion } from '../../../src-tauri/bindings/featur
 import type { ReplaceSuggestionsMessage } from '../../../src-tauri/bindings/features/refactoring/ReplaceSuggestionsMessage';
 
 export const filter_and_sort_suggestions = (
-	suggestions: ReplaceSuggestionsMessage['suggestions'],
-	active_window_uid: number | null
+  suggestions: ReplaceSuggestionsMessage['suggestions'],
+  active_window_uid: number | null
 ): [string, FERefactoringSuggestion][] => {
-	if (!active_window_uid) {
-		return [];
-	}
-	return Object.entries(suggestions[active_window_uid] ?? {})
-		.filter(([_, value]) => value.state !== 'New')
-		.sort((a, b) => a[1].start_index - b[1].start_index);
+  if (!active_window_uid) {
+    return [];
+  }
+  return Object.entries(suggestions[active_window_uid] ?? {})
+    .filter(([_, value]) => value.state !== 'New')
+    .sort((a, b) => a[1].start_index - b[1].start_index);
 };
 
 export const every_suggestion_is_new = (
-	suggestions: ReplaceSuggestionsMessage['suggestions'],
-	active_window_uid: number | null
+  suggestions: ReplaceSuggestionsMessage['suggestions'],
+  active_window_uid: number | null
 ): boolean => {
-	if (!active_window_uid) {
-		return false;
-	}
-	let suggestions_for_window = Object.entries(suggestions[active_window_uid] ?? {});
-	return (
-		suggestions_for_window.length > 0 &&
-		suggestions_for_window.every(([_, suggestion]) => suggestion.state == 'New')
-	);
+  if (!active_window_uid) {
+    return false;
+  }
+  let suggestions_for_window = Object.entries(suggestions[active_window_uid] ?? {});
+  return (
+    suggestions_for_window.length > 0 &&
+    suggestions_for_window.every(([_, suggestion]) => suggestion.state == 'New')
+  );
 };
